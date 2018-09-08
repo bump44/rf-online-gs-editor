313932608579e5be218f3e932aea1f64a01088c6
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-08 18:55:44 +0300

    initial

---
+++ src/app.js
@@ -0,0 +1,62 @@
+/**
+ * app.js
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
+
+// Needed for redux-saga es6 generator support
+import 'babel-polyfill';
+
+// Import all the third party stuff
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createMemoryHistory';
+
+import App from './containers/App';
+import LanguageProvider from './containers/LanguageProvider';
+import configureStore from './configureStore';
+import { translationMessages } from './i18n';
+
+// Import CSS reset and Global Styles
+import './global-styles';
+
+// Create redux store with history
+const initialState = {};
+const history = createHistory();
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('app');
+
+const render = messages => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <LanguageProvider messages={messages}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </LanguageProvider>
+    </Provider>,
+    MOUNT_NODE,
+  );
+};
+
+if (module.hot) {
+  // Hot reloadable React components and translation json files
+  // modules.hot.accept does not accept dynamic dependencies,
+  // have to be constants at compile-time
+  module.hot.accept(['./i18n', './containers/App'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    render(translationMessages);
+  });
+}
+
+render(translationMessages);
+
+// Install ServiceWorker and AppCache in the end since
+// it's not most important operation and if main code fails,
+// we do not want it installed
+if (process.env.NODE_ENV === 'production') {
+  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
+}
---
+++ src/components/Button/index.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Button
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Button() {
+  return (
+    <div>
+      <span />
+    </div>
+  );
+}
+
+Button.propTypes = {};
+
+export default Button;
