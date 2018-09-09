1255a2e7c455dc4594758e60321f443e7ca40177
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-09 17:51:45 +0300

    Redirect if authorized

--- src/containers/RegisterPage/index.js
+++ src/containers/RegisterPage/index.js
@@ -6,11 +6,13 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Map } from 'immutable';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import { push } from 'react-router-redux';
 
 import injectSaga from '../../utils/injectSaga';
 import injectReducer from '../../utils/injectReducer';
@@ -18,15 +20,30 @@ import makeSelectRegisterPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
+import * as actions from './actions';
+
+import { makeSelectIsLoggedIn, makeSelectCurrentUser } from '../App/selectors';
 
 import Header from '../../components/Header';
 import Button from '../../components/Button';
 import Notification from '../../components/Notification';
 import FormWrapper from './FormWrapper';
-import * as actions from './actions';
 
 /* eslint-disable react/prefer-stateless-function */
 export class RegisterPage extends React.PureComponent {
+  componentDidMount() {
+    this.redirectToMainPageIfIsLoggedIn(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.redirectToMainPageIfIsLoggedIn(nextProps);
+  }
+
+  redirectToMainPageIfIsLoggedIn(props) {
+    const { isLoggedIn, changeLocation } = props;
+    if (isLoggedIn) changeLocation('/');
+  }
+
   renderField({
     label,
     icon,
@@ -62,6 +79,8 @@ export class RegisterPage extends React.PureComponent {
       changeEmail,
       changePassword,
       submit,
+      isLoggedIn,
+      currentUser,
     } = this.props;
 
     const {
@@ -80,7 +99,7 @@ export class RegisterPage extends React.PureComponent {
           <meta name="description" content="Description of RegisterPage" />
         </Helmet>
 
-        <Header />
+        <Header isLoggedIn={isLoggedIn} currentUser={currentUser} />
 
         <div className="container is-fluid p-10">
           <FormWrapper>
@@ -136,11 +155,20 @@ RegisterPage.propTypes = {
   changeLogin: PropTypes.func.isRequired,
   changeEmail: PropTypes.func.isRequired,
   changePassword: PropTypes.func.isRequired,
+  changeLocation: PropTypes.func.isRequired,
   submit: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  currentUser: PropTypes.instanceOf(Map),
+};
+
+RegisterPage.defaultProps = {
+  currentUser: null,
 };
 
 const mapStateToProps = createStructuredSelector({
   registerPage: makeSelectRegisterPage(),
+  isLoggedIn: makeSelectIsLoggedIn(),
+  currentUser: makeSelectCurrentUser(),
 });
 
 function mapDispatchToProps(dispatch) {
@@ -149,6 +177,7 @@ function mapDispatchToProps(dispatch) {
     changeLogin: evt => dispatch(actions.changeLogin(evt.target.value)),
     changeEmail: evt => dispatch(actions.changeEmail(evt.target.value)),
     changePassword: evt => dispatch(actions.changePassword(evt.target.value)),
+    changeLocation: (location = '/') => dispatch(push(location)),
     submit: () => dispatch(actions.submit()),
   };
 }
