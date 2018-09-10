6491aef3078d4a679129ddd01cee3f25bc206a56
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-11 02:10:19 +0300

    Projects imports actions

--- src/containers/App/actions.js
+++ src/containers/App/actions.js
@@ -1,7 +1,10 @@
+import pull from 'lodash/pull';
+
 import {
   CHANGE_CURRENT_USER,
   CHANGE_CURRENT_USER_TOKEN,
   LOGOUT_CURRENT_USER,
+  PROJECTS_IMPORTS_CHANGE_PROP_VALUE,
 } from './constants';
 
 export function changeCurrentUser(user) {
@@ -23,3 +26,56 @@ export function logoutCurrentUser() {
     type: LOGOUT_CURRENT_USER,
   };
 }
+
+export const projectsImports = {
+  changePropValue({ projectId, fileKey, propKey, propValue }) {
+    return {
+      type: PROJECTS_IMPORTS_CHANGE_PROP_VALUE,
+      projectId,
+      fileKey,
+      propKey,
+      propValue,
+    };
+  },
+  changeStatus(args) {
+    return projectsImports.changePropValue({
+      ...args,
+      propKey: 'status',
+    });
+  },
+  changeImportType(args) {
+    return projectsImports.changePropValue({
+      ...args,
+      propKey: 'importType',
+    });
+  },
+};
+
+export const projectsImportsActionNames = pull(
+  Object.keys(projectsImports),
+  'changePropValue',
+);
+
+export const projectsImportsBindActions = ({ projectId, dispatch }) => {
+  const nextFns = {};
+  projectsImportsActionNames.forEach(actionKey => {
+    const actionFn = projectsImports[actionKey];
+    nextFns[actionKey] = (fileKey, propValue) =>
+      dispatch(actionFn({ projectId, fileKey, propValue }));
+  });
+  return nextFns;
+};
+
+export const projectsImportsBindActionsWithFileKey = ({
+  projectId,
+  fileKey,
+  dispatch,
+}) => {
+  const nextFns = {};
+  projectsImportsActionNames.forEach(actionKey => {
+    const actionFn = projectsImports[actionKey];
+    nextFns[actionKey] = propValue =>
+      dispatch(actionFn({ projectId, fileKey, propValue }));
+  });
+  return nextFns;
+};
