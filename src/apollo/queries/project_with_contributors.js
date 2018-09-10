e61b5d0bc9741f637bd15b2e9c8fe6b4c882dc21
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-10 19:45:08 +0300

    Project query with all contributors (todo)

---
+++ src/apollo/queries/project_with_contributors.js
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  query($id: String!) {
+    project(id: $id) {
+      title
+      description
+      name
+      id
+      createdAt
+      updatedAt
+      isPublic
+      owner {
+        id
+        login
+        role {
+          id
+          name
+          title
+        }
+      }
+    }
+  }
+`;
