edef0c6c2623e41bde1468d6f2db726a825e2ff1
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-10 16:40:36 +0300

    Edf utils

---
+++ src/utils/edf.js
@@ -0,0 +1,78 @@
+import path from 'path';
+import Promise from 'bluebird';
+import { execFile } from 'child_process';
+import { mkdir, readFile, writeFile } from './fs';
+import { getRandomTmpPath } from './path';
+import { words } from './random';
+
+const cloneExe = (tmpPath, exePath) =>
+  readFile(exePath).then(buf => {
+    const name = words(10);
+    const newFilePath = path.resolve(tmpPath, `${name}.exe`);
+    return writeFile(newFilePath, buf);
+  });
+
+const createEdfByBuf = (tmpPath, buf) => {
+  const name = words(10);
+  const newFilePath = path.resolve(tmpPath, `${name}.edf`);
+  return writeFile(newFilePath, buf);
+};
+
+const createDatByBuf = (tmpPath, buf) => {
+  const name = words(10);
+  const newFilePath = path.resolve(tmpPath, `${name}.dat`);
+  return writeFile(newFilePath, buf);
+};
+
+// return dat buf
+const execEdfToDat = (exePath, edfPath) => {
+  const pathData = path.parse(edfPath);
+  return new Promise((resolve, reject) => {
+    execFile(exePath, [`${edfPath}`], err => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve();
+    });
+  }).then(() => readFile(path.resolve(pathData.dir, `${pathData.name}.dat`)));
+};
+
+// return edf buf
+const execDatToEdf = (exePath, datPath) => {
+  const pathData = path.parse(datPath);
+  return new Promise((resolve, reject) => {
+    execFile(exePath, [`${datPath}`], err => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve();
+    });
+  }).then(() =>
+    readFile(path.resolve(pathData.dir, `${pathData.name}.dat.edf`)),
+  );
+};
+
+export const deCryptByPath = edfPath =>
+  readFile(edfPath).then(buf => deCryptByBuf(buf));
+
+export const deCryptByBuf = edfBuf => {
+  const tmpPath = getRandomTmpPath();
+  const EdfDeCrypt = path.resolve('./', 'resources/EdfDeCrypt.exe');
+
+  return mkdir(tmpPath)
+    .then(() => cloneExe(tmpPath, EdfDeCrypt))
+    .then(newExePath => [newExePath, createEdfByBuf(tmpPath, edfBuf)])
+    .spread((exePath, edfPath) => execEdfToDat(exePath, edfPath));
+};
+
+export const enCryptByBuf = datBuf => {
+  const tmpPath = getRandomTmpPath();
+  const EdfCrypt = path.resolve('./', 'resources/EdfCrypt.exe');
+
+  return mkdir(tmpPath)
+    .then(() => cloneExe(tmpPath, EdfCrypt))
+    .then(newExePath => [newExePath, createDatByBuf(tmpPath, datBuf)])
+    .spread((exePath, datPath) => execDatToEdf(exePath, datPath));
+};
