5d6858fd4fbdbceeb4f4a084c3b28b512133f274
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-12 02:34:33 +0300

    process data

--- src/containers/App/selectors.js
+++ src/containers/App/selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import { PROCESSING } from './constants';
 
 const selectRoute = state => state.get('route');
 const selectGlobal = state => state.get('global');
@@ -17,9 +18,69 @@ const makeSelectProjectsImports = () =>
     globalState.get('projectsImports'),
   );
 
+const makeSelectProjectsImportsIsProcessing = () =>
+  createSelector(selectGlobal, globalState =>
+    globalState
+      .get('projectsImports')
+      .some(fileDataSets =>
+        fileDataSets.some(
+          fileDataSet => fileDataSet.get('status') === PROCESSING,
+        ),
+      ),
+  );
+
+const makeSelectProjectsImportsProcessingData = () =>
+  createSelector(selectGlobal, globalState => {
+    const isProcessing = globalState
+      .get('projectsImports')
+      .some(fileDataSets =>
+        fileDataSets.some(
+          fileDataSet => fileDataSet.get('status') === PROCESSING,
+        ),
+      );
+
+    let countTotal = 0;
+    let countCompleted = 0;
+    let countProcesses = 0;
+
+    if (!isProcessing) {
+      return {
+        isProcessing,
+        countTotal,
+        countCompleted,
+        countProcesses,
+        percent: 0,
+      };
+    }
+
+    globalState.get('projectsImports').forEach(fileDataSets =>
+      fileDataSets.forEach(fileDataSet => {
+        if (fileDataSet.get('status') === PROCESSING) {
+          countTotal += fileDataSet.get('countTotal', 0);
+          countCompleted += fileDataSet.get('countCompleted', 0);
+          countProcesses += 1;
+        }
+      }),
+    );
+
+    return {
+      isProcessing,
+      countTotal,
+      countCompleted,
+      countProcesses,
+      percent: (() => {
+        if (countTotal <= 0) return 0;
+        if (countCompleted >= countTotal) return 100;
+        return (countCompleted / countTotal) * 100;
+      })(),
+    };
+  });
+
 export {
   makeSelectLocation,
   makeSelectCurrentUser,
   makeSelectIsLoggedIn,
   makeSelectProjectsImports,
+  makeSelectProjectsImportsIsProcessing,
+  makeSelectProjectsImportsProcessingData,
 };
