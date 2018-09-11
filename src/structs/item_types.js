ab37f7315dfe7f750d05b859685a73fe6d03fa89
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-11 23:24:55 +0300

    +armor types

--- src/structs/item_types.js
+++ src/structs/item_types.js
@@ -1,2 +1,6 @@
 export const FACE = 'face';
 export const UPPER = 'upper';
+export const LOWER = 'lower';
+export const GAUNTLET = 'gauntlet';
+export const SHOE = 'shoe';
+export const HELMET = 'helmet';
