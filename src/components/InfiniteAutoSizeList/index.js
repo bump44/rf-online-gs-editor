b11aeacafbc8130142fde0d59bbdb79403cdcb54
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-13 02:35:42 +0300

    infinite autosize list component

---
+++ src/components/InfiniteAutoSizeList/index.js
@@ -0,0 +1,66 @@
+/**
+ *
+ * InfiniteAutoSizeList
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { InfiniteLoader, AutoSizer, List } from 'react-virtualized';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class InfiniteAutoSizeList extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.forceUpdateGrid = () =>
+      this.list ? this.list.forceUpdateGrid() : false;
+  }
+
+  render() {
+    const {
+      rowCount,
+      isRowLoaded,
+      loadMoreRows,
+      rowHeight,
+      rowRenderer,
+    } = this.props;
+
+    return (
+      <InfiniteLoader
+        rowCount={rowCount}
+        isRowLoaded={isRowLoaded}
+        loadMoreRows={loadMoreRows}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <AutoSizer>
+            {({ height, width }) => (
+              <List
+                ref={list => {
+                  this.list = list;
+                  return registerChild;
+                }}
+                width={width}
+                height={height}
+                rowCount={rowCount}
+                rowHeight={rowHeight}
+                onRowsRendered={onRowsRendered}
+                rowRenderer={rowRenderer}
+              />
+            )}
+          </AutoSizer>
+        )}
+      </InfiniteLoader>
+    );
+  }
+}
+
+InfiniteAutoSizeList.propTypes = {
+  rowCount: PropTypes.number.isRequired,
+  rowHeight: PropTypes.number.isRequired,
+  isRowLoaded: PropTypes.func.isRequired,
+  loadMoreRows: PropTypes.func.isRequired,
+  rowRenderer: PropTypes.func.isRequired,
+};
+
+export default InfiniteAutoSizeList;
