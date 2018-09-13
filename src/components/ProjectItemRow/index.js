a867762972f948cbf7b926ac49e5bbc0680503c3
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-13 18:15:38 +0300

    [#todo]

--- src/components/ProjectItemRow/index.js
+++ src/components/ProjectItemRow/index.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import { Map, List } from 'immutable';
 // import styled from 'styled-components';
 
@@ -14,15 +15,81 @@ import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 class ProjectItemRow extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.renderTagIndexWithNextState = this.renderTagIndexWithNextState.bind(
+      this,
+    );
+  }
+
+  renderTagIndexWithNextState() {
+    const { item, itemNextValues } = this.props;
+    const isSaved = itemNextValues.getIn(['isSaved'], true);
+    const isSaving = itemNextValues.getIn(['isSaving'], false);
+    const isError = itemNextValues.getIn(['isError'], false);
+    const errorMessage = itemNextValues.getIn(['errorMessage'], '');
+    const nIndex = itemNextValues.getIn(
+      ['nextValue', 'nIndex'],
+      item.get('nIndex'),
+    );
+
+    const isShowStatus =
+      (!isSaving && !isSaved && !isError) || isSaving || isError;
+
+    return (
+      <span
+        title={isError ? errorMessage : undefined}
+        className={cx('tag', {
+          'is-danger': isError,
+          'is-success':
+            itemNextValues.getIn(['isSaved']) !== undefined && isSaved,
+          'is-warning': isSaving,
+        })}
+      >
+        {isShowStatus && (
+          <small>
+            <i
+              className={cx('fas', {
+                'fa-pencil-alt': !isSaving && !isSaved && !isError,
+                'fa-spin fa-spinner': isSaving,
+                'fa-times': isError,
+              })}
+            />
+            &nbsp;
+          </small>
+        )}
+        {nIndex}
+      </span>
+    );
+  }
+
   render() {
-    const { item } = this.props;
+    const { item, itemNextValues, actions } = this.props;
+
+    const strName =
+      itemNextValues.getIn(['nextValue', 'clientNd', 'strName']) ||
+      item.getIn(
+        [
+          ['priorStrName'],
+          ['clientNd', 'strName'],
+          ['serverStr', 'strNameEN'],
+          ['serverStr', 'strNameGLOBAL'],
+          ['client', 'strName'],
+          ['server', 'strName'],
+        ].find(fieldSets => !!item.getIn(fieldSets)) || 'priorStrName',
+        '',
+      );
 
     return (
       <div className="columns">
+        <div className="column">{this.renderTagIndexWithNextState()}</div>
         <div className="column">
-          <span className="tag">{item.get('nIndex')}</span>
+          <input
+            type="text"
+            value={strName}
+            onChange={evt => actions.changeName(item, evt.target.value)}
+          />
         </div>
-        <div className="column">{item.getIn(['client', 'strName'])}</div>
         <div className="column">
           <FormattedMessage {...messages.header} />
         </div>
@@ -33,7 +100,11 @@ class ProjectItemRow extends React.PureComponent {
 
 ProjectItemRow.propTypes = {
   item: PropTypes.instanceOf(Map).isRequired,
+  itemNextValues: PropTypes.instanceOf(Map).isRequired,
   items: PropTypes.instanceOf(List).isRequired,
+  actions: PropTypes.shape({
+    changeName: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default ProjectItemRow;
--- src/components/ProjectItemVirtualizedRow/index.js
+++ src/components/ProjectItemVirtualizedRow/index.js
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 import styled from 'styled-components';
 
 import ProjectItemRow from '../ProjectItemRow';
@@ -14,13 +14,21 @@ import ProjectItemRow from '../ProjectItemRow';
 /* eslint-disable react/prefer-stateless-function */
 class ProjectItemVirtualizedRow extends React.PureComponent {
   render() {
-    const { index, style, items } = this.props;
+    const { index, style, items, actions, nextValues } = this.props;
     const item = items.get(index);
 
     return (
       <div style={style}>
         <Row>
-          {item && <ProjectItemRow items={items} item={item} />}
+          {item && (
+            <ProjectItemRow
+              nextValues={nextValues}
+              actions={actions}
+              items={items}
+              item={item}
+              itemNextValues={nextValues.get(item.get('id'), Map({}))}
+            />
+          )}
           {!item && (
             <span className="tag is-warning">
               <i className="fas fa-spin fa-spinner" />
@@ -36,6 +44,8 @@ ProjectItemVirtualizedRow.propTypes = {
   index: PropTypes.number.isRequired,
   style: PropTypes.object.isRequired,
   items: PropTypes.instanceOf(List).isRequired,
+  nextValues: PropTypes.instanceOf(Map).isRequired,
+  actions: PropTypes.object.isRequired,
 };
 
 export default ProjectItemVirtualizedRow;
