616022dc20aa9c3095c7758ee04b1f8d34a5ae56
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-16 15:39:37 +0300

    count columns constant

--- src/classes/constants.js
+++ src/classes/constants.js
@@ -3,3 +3,4 @@ export const TOTAL_SIZE = 'nTotalSize';
 export const OFFSET = 'nOffset';
 export const COUNT = 'nCount';
 export const BLOCK_SIZE = 'nBlockSize';
+export const COUNT_COLUMNS = 'nCountColumns';
