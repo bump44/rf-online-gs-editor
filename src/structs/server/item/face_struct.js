76dafc1f9b6bf7856c781d30df532e92db2387be
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-16 15:40:15 +0300

    add face struct

---
+++ src/structs/server/item/face_struct.js
@@ -0,0 +1,13 @@
+import Struct from '../../../classes/Struct';
+
+export default new Struct().fromSchema1([
+  { child: { type: Number, name: 'nIndex', len: 32 } },
+  { child: { type: String, name: 'strCode', len: 64 } },
+  { child: { type: Boolean, name: 'bExist', len: 32 } },
+  { child: { type: String, name: 'strModel', len: 64 } },
+  { child: { type: String, name: 'strName', len: 64 } },
+  { child: { type: Number, name: 'nKindClt', len: 32 } },
+  { child: { type: Number, name: 'nFixPart', len: 32 } },
+  { child: { type: String, name: 'strCivil', len: 64 } },
+  { child: { type: Number, name: 'nDefEffType', len: 32 } },
+]);
