5de75221a9b266785f26a4001ebbbbb4836755de
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-17 19:31:33 +0300

    add makeData struct

---
+++ src/structs/client/item/makeData_struct.js
@@ -0,0 +1,23 @@
+import Struct from '../../../classes/Struct';
+
+export default new Struct().fromSchema1([
+  { child: { type: Number, name: 'nIndex', len: 32 } },
+  { child: { type: Number, name: 'nNewItemType', len: 32 } },
+  { child: { type: Number, name: 'nNewItemIndex', len: 32 } },
+  { child: { type: String, name: 'strNewItemCode', len: 32, as: 'hex' } },
+  { child: { type: Number, name: 'nLink', len: 32 } },
+  { child: { type: Number, name: 'nPosInWindow', len: 32 } },
+  { child: { type: String, name: 'strCivil1', len: 32, as: 'hex' } },
+  { child: { type: String, name: 'strCivil2', len: 32, as: 'hex' } },
+  { child: { type: Number, name: 'nUnkInt8_1', len: 8 } },
+  { child: { type: Number, name: 'nNumMaterials', len: 8 } },
+  { child: { type: Number, name: 'nMaterialType', len: 8 }, repeat: 5 },
+  { child: { type: Number, name: 'nUnkInt8_2', len: 8 } },
+  { child: { type: Number, name: 'nMaterialIndex', len: 32 }, repeat: 5 },
+  {
+    child: { type: String, name: 'nMaterialCode', len: 32, as: 'hex' },
+    repeat: 5,
+  },
+  { child: { type: Number, name: 'nMaterialCount', len: 32 }, repeat: 5 },
+  { child: { type: Number, name: 'nUnkInt32_1', len: 32 } },
+]);
