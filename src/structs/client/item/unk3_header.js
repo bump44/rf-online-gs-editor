b32989f2bbfc8fb994d8477888e4de5e8382c642
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-17 19:33:25 +0300

    add unk3 header & struct

---
+++ src/structs/client/item/unk3_header.js
@@ -0,0 +1,8 @@
+import Field from '../../../classes/Field';
+import { ORDER, TOTAL_SIZE, OFFSET } from '../../../classes/constants';
+
+export default [
+  new Field({ name: ORDER, type: Number, len: 16 }),
+  new Field({ name: TOTAL_SIZE, type: Number, len: 32 }),
+  new Field({ name: OFFSET, type: Number, len: 32 }),
+];
---
+++ src/structs/client/item/unk3_struct.js
@@ -0,0 +1,5 @@
+import Struct from '../../../classes/Struct';
+
+export default new Struct().fromSchema1([
+  { child: { type: Number, name: 'nUnkInt32', len: 32 }, repeat: 46 },
+]);
