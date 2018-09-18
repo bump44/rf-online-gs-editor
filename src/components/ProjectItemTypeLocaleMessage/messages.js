717a4fccfb43153cab347a2ce3ba0cf3ae8936ad
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-18 08:07:26 +0300

    add type messages

--- src/components/ProjectItemTypeLocaleMessage/messages.js
+++ src/components/ProjectItemTypeLocaleMessage/messages.js
@@ -35,4 +35,168 @@ export default defineMessages({
     id: 'app.components.ProjectItemTypeLocaleMessage.shoe',
     defaultMessage: 'shoe',
   },
+  weapon: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.weapon',
+    defaultMessage: 'weapon',
+  },
+  shield: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.shield',
+    defaultMessage: 'shield',
+  },
+  cloak: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.cloak',
+    defaultMessage: 'cloak',
+  },
+  ring: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.ring',
+    defaultMessage: 'ring',
+  },
+  amulet: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.amulet',
+    defaultMessage: 'amulet',
+  },
+  bullet: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.bullet',
+    defaultMessage: 'bullet',
+  },
+  maketool: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.maketool',
+    defaultMessage: 'maketool',
+  },
+  potion: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.potion',
+    defaultMessage: 'potion',
+  },
+  bag: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.bag',
+    defaultMessage: 'bag',
+  },
+  battery: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.battery',
+    defaultMessage: 'battery',
+  },
+  ore: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.ore',
+    defaultMessage: 'ore',
+  },
+  resource: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.resource',
+    defaultMessage: 'resource',
+  },
+  force: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.force',
+    defaultMessage: 'force',
+  },
+  unitkey: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.unitkey',
+    defaultMessage: 'unitkey',
+  },
+  booty: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.booty',
+    defaultMessage: 'booty',
+  },
+  map: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.map',
+    defaultMessage: 'map',
+  },
+  town: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.town',
+    defaultMessage: 'town',
+  },
+  dungeonkey: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.dungeonkey',
+    defaultMessage: 'dungeonkey',
+  },
+  animus: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.animus',
+    defaultMessage: 'animus',
+  },
+  tower: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.tower',
+    defaultMessage: 'tower',
+  },
+  trap: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.trap',
+    defaultMessage: 'trap',
+  },
+  siegekit: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.siegekit',
+    defaultMessage: 'siegekit',
+  },
+  ticket: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.ticket',
+    defaultMessage: 'ticket',
+  },
+  quest: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.quest',
+    defaultMessage: 'quest',
+  },
+  recovery: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.recovery',
+    defaultMessage: 'recovery',
+  },
+  box: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.box',
+    defaultMessage: 'box',
+  },
+  firecracker: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.firecracker',
+    defaultMessage: 'firecracker',
+  },
+  cashmining: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.cashmining',
+    defaultMessage: 'cashmining',
+  },
+  radar: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.radar',
+    defaultMessage: 'radar',
+  },
+  pager: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.pager',
+    defaultMessage: 'pager',
+  },
+  coupon: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.coupon',
+    defaultMessage: 'coupon',
+  },
+  mauhead: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.mauhead',
+    defaultMessage: 'mauhead',
+  },
+  mauupper: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.mauupper',
+    defaultMessage: 'mauupper',
+  },
+  maulower: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.maulower',
+    defaultMessage: 'maulower',
+  },
+  mauarm: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.mauarm',
+    defaultMessage: 'mauarm',
+  },
+  maushoulder: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.maushoulder',
+    defaultMessage: 'maushoulder',
+  },
+  mauback: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.mauback',
+    defaultMessage: 'mauback',
+  },
+  maubullet: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.maubullet',
+    defaultMessage: 'maubullet',
+  },
+  makedata: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.makedata',
+    defaultMessage: 'makedata',
+  },
+  combinedata: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.combinedata',
+    defaultMessage: 'combinedata',
+  },
+  unk3: {
+    id: 'app.components.ProjectItemTypeLocaleMessage.unk3',
+    defaultMessage: 'unk3',
+  },
 });
