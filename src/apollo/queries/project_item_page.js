9fb973d2c57ae3ffc7920f1a80a2007bc7c9ef4b
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-20 15:53:24 +0300

    update query

---
+++ src/apollo/queries/project_item_page.js
@@ -0,0 +1,70 @@
+import gql from 'graphql-tag';
+import ProjectItemClientNameParts from '../fragments/ProjectItemClientNameParts';
+import ProjectItemServerNameParts from '../fragments/ProjectItemServerNameParts';
+
+export default gql`
+  query($id: String!, $itemId: String!) {
+    project(id: $id) {
+      title
+      description
+      name
+      id
+      createdAt
+      updatedAt
+      isPublic
+      owner {
+        id
+        login
+        role {
+          id
+          name
+          title
+        }
+      }
+
+      items {
+        total
+      }
+
+      moneyTypes(take: 50) {
+        items {
+          title
+          value
+          fieldName
+          valuation
+        }
+      }
+
+      itemGrades(take: 50) {
+        items {
+          title
+          value
+        }
+      }
+
+      weaponTypes(take: 50) {
+        items {
+          title
+          value
+        }
+      }
+    }
+
+    projectItem(id: $itemId) {
+      id
+      type
+      nIndex
+      searchTextValue
+      client {
+        ...ProjectItemClientNameParts
+      }
+      server {
+        ...ProjectItemServerNameParts
+      }
+    }
+  }
+
+  # include fragments
+  ${ProjectItemClientNameParts}
+  ${ProjectItemServerNameParts}
+`;
