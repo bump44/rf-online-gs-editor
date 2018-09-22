2dd2174e8901ab4efff9484c835d43328d83ef8e
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-22 17:09:33 +0300

    remove messages

--- src/containers/ProjectCreatePage/messages.js
+++ src/containers/ProjectCreatePage/messages.js
@@ -11,20 +11,4 @@ export default defineMessages({
     id: 'app.containers.ProjectCreatePage.header',
     defaultMessage: 'Creating new project',
   },
-  Submit: {
-    id: 'app.containers.ProjectCreatePage.Submit',
-    defaultMessage: 'Submit',
-  },
-  Title: {
-    id: 'app.containers.ProjectCreatePage.Title',
-    defaultMessage: 'Title',
-  },
-  Description: {
-    id: 'app.containers.ProjectCreatePage.Description',
-    defaultMessage: 'Description',
-  },
-  IsPublicProject: {
-    id: 'app.containers.ProjectCreatePage.IsPublicProject',
-    defaultMessage: 'Is public project',
-  },
 });
