c751048dc5a4b7f8614fb8b4b8ab8061dc839dfe
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-22 16:16:00 +0300

    update notification

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -12,26 +12,43 @@ import cx from 'classnames';
 /* eslint-disable react/prefer-stateless-function */
 class Notification extends React.PureComponent {
   render() {
-    const { className, children } = this.props;
+    const { className, children, type } = this.props;
 
-    return <SNotification className={cx(className)}>{children}</SNotification>;
+    return (
+      <NotificationStyled className={cx(className)} type={type}>
+        {children}
+      </NotificationStyled>
+    );
   }
 }
 
 Notification.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  type: PropTypes.oneOf(['default', 'danger']),
 };
 
 Notification.defaultProps = {
   children: null,
   className: '',
+  type: 'default',
 };
 
 export default Notification;
 
-const SNotification = styled.div.attrs({ className: 'notification' })`
+const NotificationStyled = styled.div`
+  background-color: #ddd;
+  border-radius: 4px;
+  position: relative;
+
   padding: 10px;
   line-height: 16px;
   font-size: 14px;
+
+  ${({ type }) =>
+    type === 'danger' &&
+    `
+      background-color: #ff3860;
+      color: #fff;
+    `};
 `;
