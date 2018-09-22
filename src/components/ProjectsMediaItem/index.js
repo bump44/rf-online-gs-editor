8a23c458ddff1dc7daa7ae24c8fe8f33cf7f878f
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-22 20:47:06 +0300

    project media items

---
+++ src/components/ProjectsMediaItem/index.js
@@ -0,0 +1,72 @@
+/**
+ *
+ * ProjectsMediaItem
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { Map } from 'immutable';
+import { Item, Icon } from 'semantic-ui-react';
+import styled from 'styled-components';
+import moment from 'moment';
+
+function ProjectsMediaItem({ item, currentUser }) {
+  const { title, description, name, id, createdAt, owner, isPublic } = item;
+  const isCurrentIsOwner = currentUser && currentUser.get('id') === owner.id;
+
+  return (
+    <Item>
+      <Item.Content>
+        <Item.Header as={NavLink} to={`/project/${id}`}>
+          <Icon name={isPublic ? 'unlock' : 'lock'} />
+          {title || name}
+        </Item.Header>
+
+        <Item.Meta>
+          <small className="name">#{name}</small>
+        </Item.Meta>
+        <Item.Description>{description || '—'}</Item.Description>
+        <Item.Extra>
+          <UserName active={isCurrentIsOwner}>
+            <Icon name="user" />@{owner.login}
+          </UserName>
+          <Icon name="time" />
+          {moment(createdAt).format('LLL')}
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  );
+}
+
+ProjectsMediaItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    isPublic: PropTypes.bool.isRequired,
+    owner: PropTypes.shape({
+      login: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+  currentUser: PropTypes.instanceOf(Map),
+};
+
+ProjectsMediaItem.defaultProps = {
+  currentUser: null,
+};
+
+export default ProjectsMediaItem;
+
+const UserName = styled.span`
+  padding-right: 15px;
+  ${({ active }) =>
+    active &&
+    `
+    color: green;
+    `};
+`;
---
+++ src/components/ProjectsMediaItems/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ProjectsMediaItems from '../index';
+
+describe('<ProjectsMediaItems />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
