4564a0e71df81d200dc40431564aa42b831c2792
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-22 21:20:59 +0300

    update project menu

--- src/components/ProjectMenu/index.js
+++ src/components/ProjectMenu/index.js
@@ -6,12 +6,47 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Map } from 'immutable';
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import { Menu, Label } from 'semantic-ui-react';
 
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+
+function ProjectMenuIsOwnerItems({ projectId, project }) {
+  return (
+    <React.Fragment>
+      <Menu.Item header>
+        <FormattedMessage {...messages.Manage} />
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={`/project/${projectId}/items`} exact>
+        <FormattedMessage {...messages.Items} />
+        <Label circular color="green">
+          {project.getIn(['items', 'total'], 0)}
+        </Label>
+      </Menu.Item>
+      <Menu.Item header>
+        <FormattedMessage {...messages.Administration} />
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={`/project/${projectId}/details`} exact>
+        <FormattedMessage {...messages.UpdateDetails} />
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={`/project/${projectId}/remove`} exact>
+        <FormattedMessage {...messages.RemoveProject} />
+      </Menu.Item>
+      <Menu.Item header>
+        <FormattedMessage {...messages.ImportExportFiles} />
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={`/project/${projectId}/import`} exact>
+        <FormattedMessage {...messages.ImportFiles} />
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={`/project/${projectId}/export`} exact>
+        <FormattedMessage {...messages.ExportFiles} />
+      </Menu.Item>
+    </React.Fragment>
+  );
+}
 
 function ProjectMenu({ projectId, project, currentUser }) {
   const isCurrentIsOwner =
@@ -20,69 +55,22 @@ function ProjectMenu({ projectId, project, currentUser }) {
     currentUser.get('id') === project.getIn(['owner', 'id']);
 
   return (
-    <Aside>
-      <p className="menu-label">
+    <Aside vertical>
+      <Menu.Item header>
         <FormattedMessage {...messages.General} />
-      </p>
-      <ul className="menu-list">
-        <li>
-          <Link exact to={`/project/${projectId}`}>
-            <FormattedMessage {...messages.Dashboard} />
-          </Link>
-        </li>
-        <li>
-          <Link to={`/project/${projectId}/contributors`}>
-            <FormattedMessage {...messages.Contributors} />
-          </Link>
-        </li>
-      </ul>
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={`/project/${projectId}`} exact>
+        <FormattedMessage {...messages.Dashboard} />
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={`/project/${projectId}/contributors`} exact>
+        <FormattedMessage {...messages.Contributors} />
+      </Menu.Item>
       {isCurrentIsOwner && (
-        <React.Fragment>
-          <p className="menu-label">
-            <FormattedMessage {...messages.Manage} />
-          </p>
-          <ul className="menu-list">
-            <li>
-              <Link to={`/project/${projectId}/items`}>
-                <FormattedMessage {...messages.Items} />
-                &nbsp;
-                <span className="tag is-small is-primary">
-                  {project.getIn(['items', 'total'])}
-                </span>
-              </Link>
-            </li>
-          </ul>
-          <p className="menu-label">
-            <FormattedMessage {...messages.Administration} />
-          </p>
-          <ul className="menu-list">
-            <li>
-              <Link to={`/project/${projectId}/details`}>
-                <FormattedMessage {...messages.UpdateDetails} />
-              </Link>
-            </li>
-            <li>
-              <Link to={`/project/${projectId}/remove`}>
-                <FormattedMessage {...messages.RemoveProject} />
-              </Link>
-            </li>
-          </ul>
-          <p className="menu-label">
-            <FormattedMessage {...messages.ImportExportFiles} />
-          </p>
-          <ul className="menu-list">
-            <li>
-              <Link to={`/project/${projectId}/import`}>
-                <FormattedMessage {...messages.ImportFiles} />
-              </Link>
-            </li>
-            <li>
-              <Link to={`/project/${projectId}/export`}>
-                <FormattedMessage {...messages.ExportFiles} />
-              </Link>
-            </li>
-          </ul>
-        </React.Fragment>
+        <ProjectMenuIsOwnerItems
+          projectId={projectId}
+          project={project}
+          currentUser={currentUser}
+        />
       )}
     </Aside>
   );
@@ -102,13 +90,7 @@ ProjectMenu.defaultProps = {
 
 export default ProjectMenu;
 
-const Link = styled(NavLink).attrs({ activeClassName: 'is-active' })``;
-const Aside = styled.aside.attrs({ className: 'menu' })`
-  background: #fff;
-  padding: 15px;
-  border-radius: 3px;
-  height: calc(100vh - 73.5px);
+const Aside = styled(Menu)`
+  height: calc(100vh - 78.5px);
   overflow-y: auto;
-  box-shadow: 0 -3px 31px 0 rgba(0, 0, 0, 0.05),
-    0 6px 20px 0 rgba(0, 0, 0, 0.02);
 `;
