8dabd9731eafe21a598429acc6aa6348ffcfffd0
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-24 17:33:58 +0300

    ProjectItemInteractingItemGrade

--- src/components/ProjectItem/Interacting/ItemGrade.js
+++ src/components/ProjectItem/Interacting/ItemGrade.js
@@ -6,9 +6,9 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 import { parseInt, isNumber } from 'lodash';
 import { Map, List } from 'immutable';
+import { Dropdown as DropdownUI } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
@@ -19,9 +19,9 @@ class ProjectItemInteractingItemGrade extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.changeValue = evt => {
+    this.changeValue = (evt, owns) => {
       const { onChangeValue, item } = this.props;
-      onChangeValue(item, parseInt(evt.target.value));
+      onChangeValue(item, parseInt(owns.value) || 0);
     };
   }
 
@@ -42,44 +42,59 @@ class ProjectItemInteractingItemGrade extends React.PureComponent {
     );
 
     const value = nextValue !== undefined ? nextValue : currValue;
-    const isUnknown = !types.some(val => val.get('value') === value);
+    const type = types.find(val => val.get('value') === value);
+    const isUnknown = !type;
 
     return (
-      <div className={cx('field', className)}>
-        <SelectControl
-          className="control has-icons-left"
-          itemGrade={value}
-          isUnknown={isUnknown}
-        >
-          <div className={cx('select is-small is-fullwidth')}>
-            <select
-              value={isNumber(value) ? value : undefined}
-              onChange={this.changeValue}
-            >
-              {isUnknown && (
-                <FormattedMessage {...messages.UnknownItemGrade}>
-                  {message => (
-                    <option value={value}>
-                      {isNumber(value) && `${value}: `}
-                      {message}
-                    </option>
-                  )}
-                </FormattedMessage>
-              )}
-              {types.map(val => (
-                <option value={val.get('value')} key={val.get('value')}>
-                  {val.get('value')}
-                  :&nbsp;
-                  {val.get('title')}
-                </option>
-              ))}
-            </select>
-          </div>
-          <span className="icon is-small is-left">
-            <i className="fas fa-level-up-alt" />
-          </span>
-        </SelectControl>
-      </div>
+      <Dropdown
+        text={
+          isUnknown ? (
+            <span>
+              {isNumber(value) && `${value}: `}
+              <FormattedMessage {...messages.UnknownItemGrade} />
+            </span>
+          ) : (
+            `${type.get('value')}: ${type.get('title')}`
+          )
+        }
+        inline
+        labeled
+        scrolling
+        item
+        icon="level up"
+        className={className}
+        value={value}
+        isUnknown={isUnknown}
+      >
+        <Dropdown.Menu>
+          {isUnknown && (
+            <DropdownItem
+              selected
+              isUnknown
+              text={
+                <span>
+                  {isNumber(value) && `${value}: `}
+                  <FormattedMessage {...messages.UnknownItemGrade} />
+                </span>
+              }
+            />
+          )}
+
+          {types.map(val => (
+            <DropdownItem
+              onClick={this.changeValue}
+              selected={val.get('value') === value}
+              key={val.get('value')}
+              value={val.get('value')}
+              text={
+                <span>
+                  {val.get('value')}: {val.get('title')}
+                </span>
+              }
+            />
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
     );
   }
 }
@@ -99,111 +114,151 @@ ProjectItemInteractingItemGrade.defaultProps = {
 export default ProjectItemInteractingItemGrade;
 
 /* eslint-disable indent */
-const SelectControl = styled.div`
-  .select {
-    &:not(.is-multiple):not(.is-loading)::after {
-      border-color: ${({ itemGrade }) =>
-        [1, 8].indexOf(itemGrade) !== -1 ? '#333' : '#fff'};
-      right: 1.125em;
-      z-index: 4;
-    }
-
-    &:not(.is-multiple):not(.is-loading):hover::after {
-      border-color: ${({ itemGrade }) =>
-        [1, 8].indexOf(itemGrade) !== -1 ? '#000' : '#ddd'};
-    }
-  }
-
-  select {
-    border-color: #000;
-    background: gray;
-    color: #fff;
-
-    transition: all 0.1s ease;
-  }
-
-  select,
-  &.control.has-icons-left .icon {
-    color: ${({ itemGrade }) =>
-      [1, 8].indexOf(itemGrade) !== -1 ? '#333' : '#fff'};
-  }
-
-  select:focus,
-  select.is-focused,
-  select:active,
-  select.is-active {
-    border-color: #333;
-    box-shadow: 0 0 0 0.125em rgba(0, 0, 0, 0.25);
-  }
-
-  select:hover,
-  select.is-hovered {
-    border-color: #5a5a5a;
-  }
-
-  select {
-    ${({ isUnknown }) =>
-      isUnknown &&
-      `
+const Dropdown = styled(DropdownUI)`
+  &.ui.dropdown {
+    >.text {
+      ${({ isUnknown }) =>
+        isUnknown &&
+        `
       border-color: #ce0830;
       background-color: #ce0830;
-    `};
+      `};
 
-    ${({ itemGrade }) =>
-      itemGrade === 1 &&
-      `
+      ${({ value }) =>
+        value === 1 &&
+        `
       border-color: #e4e436;
       background: #e4e436;
-    `};
+      color: #000;
+      `};
 
-    ${({ itemGrade }) =>
-      itemGrade === 2 &&
-      `
+      ${({ value }) =>
+        value === 2 &&
+        `
       border-color: #e436a6;
       background: #e436a6;
-    `}
+      color: #fff;
+      `}
 
-    ${({ itemGrade }) =>
-      itemGrade === 3 &&
-      `
+      ${({ value }) =>
+        value === 3 &&
+        `
       border-color: #e47636;
       background: #e47636;
-    `}
+      color: #fff;
+      `}
 
-    ${({ itemGrade }) =>
-      itemGrade === 4 &&
-      `
+      ${({ value }) =>
+        value === 4 &&
+        `
       border-color: #367ce4;
       background: #367ce4;
-    `}
+      color: #fff;
+      `}
 
-    ${({ itemGrade }) =>
-      itemGrade === 5 &&
-      `
+      ${({ value }) =>
+        value === 5 &&
+        `
       border-color: #ff89d5;
       background: #ff89d5;
-    `}
+      color: #000;
+      `}
 
-    ${({ itemGrade }) =>
-      itemGrade === 6 &&
-      `
+      ${({ value }) =>
+        value === 6 &&
+        `
       border-color: #ff2a2a;
       background: #ff2a2a;
-    `}
+      color: #fff;
+      `}
 
-    ${({ itemGrade }) =>
-      itemGrade === 7 &&
-      `
+      ${({ value }) =>
+        value === 7 &&
+        `
       border-color: #3acc74;
       background: #3acc74;
-    `}
+      color: #000;
+      `}
 
-    ${({ itemGrade }) =>
-      itemGrade === 8 &&
-      `
+      ${({ value }) =>
+        value === 8 &&
+        `
       border-color: #a4eac0;
       background: #a4eac0;
-    `}
+      color: #000;
+      `}
+    }
+
+    .menu>.item {
+      &.selected {
+        font-weight: bold;
+        background: inherit;
+      }
+    }
   }
+`;
+
+const DropdownItem = styled(Dropdown.Item)`
+  &.item {
+    &.selected {
+      font-weight: bold;
+    }
+  }
+
+  ${({ value }) =>
+    value === 1 &&
+    `
+  border-color: #e4e436;
+  background: #e4e436;
+  `};
+
+  ${({ value }) =>
+    value === 2 &&
+    `
+  border-color: #e436a6;
+  background: #e436a6;
+  `}
+
+  ${({ value }) =>
+    value === 3 &&
+    `
+  border-color: #e47636;
+  background: #e47636;
+  `}
+
+  ${({ value }) =>
+    value === 4 &&
+    `
+  border-color: #367ce4;
+  background: #367ce4;
+  `}
+
+  ${({ value }) =>
+    value === 5 &&
+    `
+  border-color: #ff89d5;
+  background: #ff89d5;
+  `}
+
+  ${({ value }) =>
+    value === 6 &&
+    `
+  border-color: #ff2a2a;
+  background: #ff2a2a;
+  `}
+
+  ${({ value }) =>
+    value === 7 &&
+    `
+  border-color: #3acc74;
+  background: #3acc74;
+  `}
+
+  ${({ value }) =>
+    value === 8 &&
+    `
+  border-color: #a4eac0;
+  background: #a4eac0;
+  `}
 `;
 /* eslint-enable indent */
