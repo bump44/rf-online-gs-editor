38d8980241ff1f224169332578b63d6eed0d70bf
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-24 19:36:08 +0300

    ru

--- src/i18n.js
+++ src/i18n.js
@@ -9,16 +9,20 @@
  */
 const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
 const enLocaleData = require('react-intl/locale-data/en');
+const ruLocaleData = require('react-intl/locale-data/ru');
 
 const enTranslationMessages = require('./translations/en.json');
+const ruTranslationMessages = require('./translations/ru.json');
 
 addLocaleData(enLocaleData);
+addLocaleData(ruLocaleData);
 
 const DEFAULT_LOCALE = 'en';
 
 // prettier-ignore
 const appLocales = [
   'en',
+  'ru',
 ];
 
 const formatTranslationMessages = (locale, messages) => {
@@ -38,6 +42,7 @@ const formatTranslationMessages = (locale, messages) => {
 
 const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
+  ru: formatTranslationMessages('ru', ruTranslationMessages),
 };
 
 exports.appLocales = appLocales;
