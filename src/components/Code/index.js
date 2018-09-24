49e1ddce3e813d1c71541173b5c8da0e922e8111
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-24 12:15:02 +0300

    add selectable code component

---
+++ src/components/Code/index.js
@@ -0,0 +1,58 @@
+/**
+ *
+ * Code
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Label } from 'semantic-ui-react';
+
+class Code extends React.PureComponent {
+  static propTypes = {
+    children: PropTypes.node,
+    onClick: PropTypes.func,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.createRef = this.createRef.bind(this);
+    this.onClickSelect = this.onClickSelect.bind(this);
+  }
+
+  createRef(target) {
+    this.elementRef = target;
+  }
+
+  onClickSelect(evt) {
+    evt.preventDefault();
+    const range = document.createRange();
+    const sel = window.getSelection();
+    range.selectNodeContents(this.elementRef);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+
+  render() {
+    const { children, onClick } = this.props;
+
+    return (
+      <Label>
+        <CodeStyled
+          ref={this.createRef}
+          onClick={onClick || this.onClickSelect}
+        >
+          {children}
+        </CodeStyled>
+      </Label>
+    );
+  }
+}
+
+export default Code;
+
+const CodeStyled = styled.code`
+  color: rgb(224, 57, 151);
+`;
---
+++ src/components/Code/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import Code from '../index';
+
+describe('<Code />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
