2d46c8714bf280304733ff1bcde489122a723ea8
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-24 18:09:54 +0300

    ProjectItemTypeSelect

--- src/components/ProjectItemTypeSelect/index.js
+++ src/components/ProjectItemTypeSelect/index.js
@@ -7,7 +7,6 @@
 import map from 'lodash/map';
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
 import { Dropdown } from 'semantic-ui-react';
 
 import { FormattedMessage } from 'react-intl';
@@ -16,55 +15,48 @@ import messages from './messages';
 import ProjectItemTypeLocaleMessage from '../ProjectItemTypeLocaleMessage';
 import * as itemTypes from '../../structs/item_types';
 
-const options = map(itemTypes, (type, key) => ({
-  key,
-  value: type,
-  text: <ProjectItemTypeLocaleMessage messageKey={type} upperFirst />,
-}));
-
-options.unshift({
-  key: '___',
-  value: '0',
-  text: <FormattedMessage {...messages.ItemType} />,
-});
-
 /* eslint-disable react/prefer-stateless-function */
 class ProjectItemTypeSelect extends React.PureComponent {
   render() {
     const { onChange, value } = this.props;
 
+    const MessageComponent = !value
+      ? FormattedMessage
+      : ProjectItemTypeLocaleMessage;
+
+    const MessageComponentProps = !value
+      ? messages.AllItemTypes
+      : { messageKey: value, upperFirst: true };
+
     return (
-      <Dropdown
-        selection
-        scrolling
-        text={
-          !value ? (
-            <FormattedMessage {...messages.AllItemTypes} />
-          ) : (
-            <ProjectItemTypeLocaleMessage messageKey={value} upperFirst />
-          )
-        }
-      >
-        <Dropdown.Menu>
-          <Dropdown.Item
-            value=""
-            onClick={onChange}
-            selected={!value}
-            text={<FormattedMessage {...messages.All} />}
-          />
-          {map(itemTypes, (type, key) => (
-            <Dropdown.Item
-              value={type}
-              key={key}
-              onClick={onChange}
-              selected={type === value}
-              text={
-                <ProjectItemTypeLocaleMessage messageKey={type} upperFirst />
-              }
-            />
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
+      <MessageComponent {...MessageComponentProps}>
+        {message => (
+          <Dropdown scrolling text={message}>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                value=""
+                onClick={onChange}
+                selected={!value}
+                text={<FormattedMessage {...messages.All} />}
+              />
+              {map(itemTypes, (type, key) => (
+                <Dropdown.Item
+                  value={type}
+                  key={key}
+                  onClick={onChange}
+                  selected={type === value}
+                  text={
+                    <ProjectItemTypeLocaleMessage
+                      messageKey={type}
+                      upperFirst
+                    />
+                  }
+                />
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        )}
+      </MessageComponent>
     );
   }
 }
