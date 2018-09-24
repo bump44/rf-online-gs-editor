5b2f34ddca8bc4a860d271527e735730e969bad1
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-24 17:34:38 +0300

    ProjectItemInteractingSell

--- src/components/ProjectItem/Interacting/Sell.js
+++ src/components/ProjectItem/Interacting/Sell.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Map /* , List */ } from 'immutable';
+import { Checkbox } from 'semantic-ui-react';
 // import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
@@ -17,9 +18,9 @@ class ProjectItemInteractingSell extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.changeValue = evt => {
+    this.changeValue = (evt, owns) => {
       const { onChangeValue, item } = this.props;
-      onChangeValue(item, evt.target.checked);
+      onChangeValue(item, owns.checked);
     };
   }
 
@@ -38,15 +39,16 @@ class ProjectItemInteractingSell extends React.PureComponent {
     const value = nextValue !== undefined ? nextValue : currValue;
 
     return (
-      <label className="checkbox is-small">
-        <input
-          type="checkbox"
-          value={1}
-          checked={!!value}
-          onChange={this.changeValue}
-        />
-        <FormattedMessage {...messages.Sell} />
-      </label>
+      <FormattedMessage {...messages.Sell}>
+        {message => (
+          <Checkbox
+            label={message}
+            value={1}
+            checked={!!value}
+            onChange={this.changeValue}
+          />
+        )}
+      </FormattedMessage>
     );
   }
 }
