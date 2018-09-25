013070b1140963cb488bd9564b4e44268e5231e8
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-26 01:18:31 +0300

    rm offline-plugin/runtime

--- src/app.js
+++ src/app.js
@@ -81,10 +81,3 @@ if (module.hot) {
 }
 
 render(translationMessages);
-
-// Install ServiceWorker and AppCache in the end since
-// it's not most important operation and if main code fails,
-// we do not want it installed
-if (process.env.NODE_ENV === 'production') {
-  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
-}
