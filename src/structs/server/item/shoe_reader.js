a3ac237628cb6f53c434ab63a8896fb3d5df29ab
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-26 02:37:27 +0300

    shoe reader

---
+++ src/structs/server/item/shoe_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { SHOE } from '../../item_types';
+import defaultHeader from './default_header';
+import armorStruct from './armor_struct';
+
+export default class ServerItemShoeReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemShoeReader',
+      struct,
+    });
+  }
+}
+
+const struct = [
+  {
+    type: SHOE,
+    header: defaultHeader,
+    block: armorStruct,
+  },
+];
