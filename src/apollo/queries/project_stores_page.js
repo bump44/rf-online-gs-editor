6e6cf311f7d8dc6053b2b2748126bdd6f2654b76
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-26 17:06:29 +0300

    stores page query

---
+++ src/apollo/queries/project_stores_page.js
@@ -0,0 +1,56 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  query($id: String!) {
+    project(id: $id) {
+      title
+      description
+      name
+      id
+      createdAt
+      updatedAt
+      isPublic
+
+      owner {
+        id
+        login
+        role {
+          id
+          name
+          title
+        }
+      }
+
+      items {
+        total
+      }
+
+      stores {
+        total
+      }
+
+      moneyTypes(take: 50) {
+        items {
+          title
+          value
+          fieldName
+          valuation
+        }
+      }
+
+      itemGrades(take: 50) {
+        items {
+          title
+          value
+        }
+      }
+
+      weaponTypes(take: 50) {
+        items {
+          title
+          value
+        }
+      }
+    }
+  }
+`;
