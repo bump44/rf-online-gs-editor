7486349b79b0d1908e7aac99648d85da22d11d4c
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-26 17:39:20 +0300

    view code

--- src/components/ProjectStoreRow/index.js
+++ src/components/ProjectStoreRow/index.js
@@ -9,9 +9,11 @@ import PropTypes from 'prop-types';
 import { Map, List } from 'immutable';
 import { Grid } from 'semantic-ui-react';
 
+import { AUTO_REVERSE_CLIENT_CODES } from '../../containers/App/constants';
 import ProjectStoreLabelDetail from '../ProjectStoreLabelDetail';
 import Render from './Render';
 import Row from './styles';
+import Code from '../Code';
 
 /* eslint-disable react/prefer-stateless-function */
 class ProjectStoreRow extends React.PureComponent {
@@ -46,13 +48,40 @@ class ProjectStoreRow extends React.PureComponent {
       localSettings,
     } = this.props;
 
+    const autoReverseClientCodes = localSettings.get(AUTO_REVERSE_CLIENT_CODES);
+    const serverStrCode = item.getIn(['server', 'strCode']) || '';
+    const clientStrCode = item.getIn(['client', 'strCode']) || '';
+
+    /* eslint-disable indent */
+    const clientStrCodeReversed = autoReverseClientCodes
+      ? clientStrCode
+          .split(/(.{2})/g)
+          .reverse()
+          .join('')
+      : clientStrCode;
+    /* eslint-enable indent */
+
+    const isShowStrCode = !!(serverStrCode || clientStrCodeReversed);
+
     return (
       <Row>
         <Grid columns={2}>
           <Grid.Column largeScreen={3} widescreen={2}>
             {this.renderTagIndexWithNextState()}
 
-            <div className="mt-5" />
+            <div className="mt-5">
+              {isShowStrCode && (
+                <Code
+                  title={
+                    serverStrCode && clientStrCodeReversed
+                      ? clientStrCodeReversed
+                      : undefined
+                  }
+                >
+                  {serverStrCode || clientStrCodeReversed}
+                </Code>
+              )}
+            </div>
           </Grid.Column>
           <Grid.Column largeScreen={13} widescreen={14}>
             {Render && (
