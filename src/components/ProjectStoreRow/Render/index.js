1e70a0d6cf128a1e46ea9ec3566785eb6cf4ebff
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-26 19:45:40 +0300

    ProjectStoreRow

--- src/components/ProjectStoreRow/Render/index.js
+++ src/components/ProjectStoreRow/Render/index.js
@@ -7,15 +7,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Map /* , List */ } from 'immutable';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Label } from 'semantic-ui-react';
+
+import { FormattedMessage } from 'react-intl';
+import messages from '../messages';
+
+import * as projectStore from '../../../containers/App/getters/projectStore';
 
 import ProjectStoreInteractingName from '../../ProjectStore/Interacting/Name';
 import ProjectStoreInteractingLastName from '../../ProjectStore/Interacting/LastName';
+import ProjectStoreInteractingTrade from '../../ProjectStore/Interacting/Trade';
+import ProjectStoreInteractingUseAngle from '../../ProjectStore/Interacting/UseAngle';
+import ProjectStoreInteractingSize from '../../ProjectStore/Interacting/Size';
+import ProjectStoreInteractingAngle from '../../ProjectStore/Interacting/Angle';
 
 /* eslint-disable react/prefer-stateless-function */
 class ProjectItemRowRender extends React.PureComponent {
   render() {
     const { item, itemNextValues, actions } = this.props;
+
+    const bUseAngle = projectStore.getUseAngle(
+      itemNextValues.get('nextValue'),
+      { item },
+    );
 
     return (
       <Grid columns={3}>
@@ -29,8 +43,56 @@ class ProjectItemRowRender extends React.PureComponent {
           <ProjectStoreInteractingLastName
             item={item}
             itemNextValues={itemNextValues}
-            onChangeValue={actions.changeName}
+            onChangeValue={actions.changeLastName}
           />
+        </Grid.Column>
+        <Grid.Column largeScreen={12} widescreen={11}>
+          <Grid columns={2}>
+            <Grid.Column>
+              <ProjectStoreInteractingTrade
+                item={item}
+                itemNextValues={itemNextValues}
+                onChangeValue={actions.changeTrade}
+                className="pb-10"
+                label={
+                  <Label>
+                    <FormattedMessage {...messages.Trade} />
+                  </Label>
+                }
+              />
+              <ProjectStoreInteractingUseAngle
+                item={item}
+                itemNextValues={itemNextValues}
+                onChangeValue={actions.changeUseAngle}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <ProjectStoreInteractingSize
+                item={item}
+                itemNextValues={itemNextValues}
+                onChangeValue={actions.changeSize}
+                className="pb-5"
+                label={
+                  <Label>
+                    <FormattedMessage {...messages.Size} />
+                  </Label>
+                }
+              />
+
+              {bUseAngle && (
+                <ProjectStoreInteractingAngle
+                  item={item}
+                  itemNextValues={itemNextValues}
+                  onChangeValue={actions.changeAngle}
+                  label={
+                    <Label>
+                      <FormattedMessage {...messages.Angle} />
+                    </Label>
+                  }
+                />
+              )}
+            </Grid.Column>
+          </Grid>
         </Grid.Column>
       </Grid>
     );
@@ -43,6 +105,9 @@ ProjectItemRowRender.propTypes = {
   // moneyTypes: PropTypes.instanceOf(List).isRequired,
   actions: PropTypes.shape({
     changeName: PropTypes.func.isRequired,
+    changeLastName: PropTypes.func.isRequired,
+    changeTrade: PropTypes.func.isRequired,
+    changeUseAngle: PropTypes.func.isRequired,
   }).isRequired,
 };
 
--- src/components/ProjectStoreRow/messages.js
+++ src/components/ProjectStoreRow/messages.js
@@ -11,4 +11,16 @@ export default defineMessages({
     id: 'app.components.ProjectStoreRow.header',
     defaultMessage: 'This is the ProjectStoreRow component !',
   },
+  Trade: {
+    id: 'app.components.ProjectStoreRow.Trade',
+    defaultMessage: 'Vendor trade type',
+  },
+  Size: {
+    id: 'app.components.ProjectStoreRow.Size',
+    defaultMessage: 'Vendor size',
+  },
+  Angle: {
+    id: 'app.components.ProjectStoreRow.Angle',
+    defaultMessage: 'Vendor angle',
+  },
 });
