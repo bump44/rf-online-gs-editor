99b2ca16910c186821ae0997d745cfa6ad9823db
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-26 19:44:37 +0300

    ProjectStoreInteractingAngle

---
+++ src/components/ProjectStore/Interacting/Angle.js
@@ -0,0 +1,65 @@
+/**
+ *
+ * ProjectStoreInteractingAngle
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Map } from 'immutable';
+import { Input } from 'semantic-ui-react';
+import * as projectStore from '../../../containers/App/getters/projectStore';
+
+/* eslint-disable react/prefer-stateless-function */
+class ProjectStoreInteractingAngle extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.changeValue = evt => {
+      const { onChangeValue, item } = this.props;
+      onChangeValue(item, parseFloat(evt.target.value) || 1);
+    };
+  }
+
+  render() {
+    const { item, itemNextValues, size, className, label, fluid } = this.props;
+    const value = projectStore.getAngle(itemNextValues.get('nextValue'), {
+      item,
+    });
+
+    return (
+      <Input
+        size={size}
+        fluid={fluid}
+        type="text"
+        value={value}
+        onChange={this.changeValue}
+        className={className}
+        label={label}
+      />
+    );
+  }
+}
+
+ProjectStoreInteractingAngle.propTypes = {
+  item: PropTypes.instanceOf(Map).isRequired,
+  itemNextValues: PropTypes.instanceOf(Map).isRequired,
+  onChangeValue: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(['mini', 'small', 'large', 'big', 'huge', 'massive']),
+  className: PropTypes.string,
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.node,
+    PropTypes.number,
+  ]),
+  fluid: PropTypes.bool,
+};
+
+ProjectStoreInteractingAngle.defaultProps = {
+  size: 'mini',
+  className: '',
+  label: null,
+  fluid: true,
+};
+
+export default ProjectStoreInteractingAngle;
