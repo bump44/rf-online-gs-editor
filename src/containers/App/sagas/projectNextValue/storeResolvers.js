48a7e080869a63ff0722ea4db10f4ffb5dfc4007
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-27 15:05:32 +0300

    store changes

--- src/containers/App/sagas/projectNextValue/storeResolvers.js
+++ src/containers/App/sagas/projectNextValue/storeResolvers.js
@@ -2,12 +2,16 @@ const Resolvers = {
   name: (item, nextValue) =>
     item
       .setIn(['priorStrName'], nextValue)
-      .setIn(['client', 'strStoreNPCname'], nextValue),
+      .setIn(['client', 'strStoreNPCname'], nextValue)
+      .setIn(['server', 'strStoreNPCname'], nextValue),
 
   lastName: (item, nextValue) =>
     item.setIn(['client', 'strStoreNPClastName'], nextValue),
 
-  trade: (item, nextValue) => item.setIn(['client', 'nStoreTrade'], nextValue),
+  trade: (item, nextValue) =>
+    item
+      .setIn(['server', 'nStoreTrade'], nextValue)
+      .setIn(['client', 'nStoreTrade'], nextValue),
 
   useAngle: (item, nextValue) =>
     item.setIn(['client', 'bSetNPCangle'], !!nextValue),
