22f414403ab628d82cd4fb1ae735483bacceea79
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-29 02:04:13 +0300

    finder saga

--- src/containers/App/saga.js
+++ src/containers/App/saga.js
@@ -1,7 +1,12 @@
 import { all, fork } from 'redux-saga/effects';
 import projectImportSaga from './sagas/projectImportSaga';
 import projectNextValueSaga from './sagas/projectNextValueSaga';
+import projectEntriesFinder from './sagas/projectEntriesFinder';
 
 export default function* defaultSaga() {
-  yield all([fork(projectImportSaga), fork(projectNextValueSaga)]);
+  yield all([
+    fork(projectImportSaga),
+    fork(projectNextValueSaga),
+    fork(projectEntriesFinder),
+  ]);
 }
---
+++ src/containers/App/sagas/projectEntriesFinder.js
@@ -0,0 +1,6 @@
+import { all, fork } from 'redux-saga/effects';
+import itemsFinder from './projectEntriesFinder/itemsFinder';
+
+export default function* defaultSaga() {
+  yield all([fork(itemsFinder)]);
+}
