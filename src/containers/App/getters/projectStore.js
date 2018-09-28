a0ecc449334ecc9a9fdbc2eaef8250cd11146d58
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-28 16:39:36 +0300

    update project store getters

--- src/containers/App/getters/projectStore.js
+++ src/containers/App/getters/projectStore.js
@@ -1,134 +1,203 @@
-import { isUndefined, isNullOrUndefined } from 'util';
+import { isNullOrUndefined } from 'util';
 import { IMMUTABLE_MAP } from '../constants';
 
-export const getName = (
-  nextValues = IMMUTABLE_MAP,
-  { item = IMMUTABLE_MAP },
-) => {
-  const nextValue = nextValues.get('priorStrName');
+/**
+ * Return the most important title of the subject
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ *
+ * @returns String
+ */
+export const getName = (nextValues = IMMUTABLE_MAP, { item = IMMUTABLE_MAP }) =>
+  nextValues.get(
+    'priorStrName',
+    item.getIn(
+      [
+        ['priorStrName'],
+        ['client', 'strStoreNPCname'],
+        ['server', 'strStoreNPCname'],
+      ].find(fieldSets => !isNullOrUndefined(item.getIn(fieldSets))) ||
+        'priorStrName',
+    ),
+  ) || '';
 
-  const currValue = item.getIn(
-    [
-      ['priorStrName'],
-      ['client', 'strStoreNPCname'],
-      ['server', 'strStoreNPCname'],
-    ].find(fieldSets => !isNullOrUndefined(item.getIn(fieldSets))) ||
-      'priorStrName',
-    '',
-  );
-
-  return !isNullOrUndefined(nextValue) ? nextValue : currValue;
-};
-
+/**
+ * Return the most important last title of the subject
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ *
+ * @returns String
+ */
 export const getLastName = (
   nextValues = IMMUTABLE_MAP,
   { item = IMMUTABLE_MAP },
-) => {
-  const nextValue = nextValues.getIn(['client', 'strStoreNPClastName']);
+) =>
+  nextValues.getIn(
+    ['client', 'strStoreNPClastName'],
+    item.getIn(
+      [['client', 'strStoreNPClastName']].find(
+        fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
+      ) || ['client', 'strStoreNPClastName'],
+    ),
+  ) || '';
 
-  const currValue = item.getIn(
-    [['client', 'strStoreNPClastName']].find(
-      fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
-    ) || ['client', 'strStoreNPClastName'],
-    '',
+/**
+ * Return vendor trade type
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ *
+ * @returns Number
+ */
+export const getTrade = (
+  nextValues = IMMUTABLE_MAP,
+  { item = IMMUTABLE_MAP },
+) =>
+  nextValues.getIn(
+    ['client', 'nStoreTrade'],
+    item.getIn(
+      [['server', 'nStoreTrade'], ['client', 'nStoreTrade']].find(
+        fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
+      ) || ['server', 'nStoreTrade'],
+    ),
+  ) || 0;
+
+/**
+ * Return use angle
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ *
+ * @returns Boolean
+ */
+export const getUseAngle = (
+  nextValues = IMMUTABLE_MAP,
+  { item = IMMUTABLE_MAP },
+) =>
+  !!nextValues.getIn(
+    ['client', 'bSetNPCangle'],
+    item.getIn(
+      [['client', 'bSetNPCangle']].find(
+        fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
+      ) || ['client', 'bSetNPCangle'],
+    ),
   );
 
-  return !isNullOrUndefined(nextValue) ? nextValue : currValue;
-};
+/**
+ * Return vendor size
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ *
+ * @returns Number
+ */
+export const getSize = (nextValues = IMMUTABLE_MAP, { item = IMMUTABLE_MAP }) =>
+  nextValues.getIn(
+    ['client', 'fStoreNPCsize'],
+    item.getIn(
+      [['client', 'fStoreNPCsize']].find(
+        fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
+      ) || ['client', 'fStoreNPCsize'],
+    ),
+  ) || 1;
 
-export const getTrade = (nextValues, { item }) => {
-  const nextValue = nextValues && nextValues.getIn(['client', 'nStoreTrade']);
+/**
+ * Return vendor angle
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ *
+ * @returns Number
+ */
+export const getAngle = (
+  nextValues = IMMUTABLE_MAP,
+  { item = IMMUTABLE_MAP },
+) =>
+  nextValues.getIn(
+    ['client', 'fStoreNPCangle'],
+    item.getIn(
+      [['client', 'fStoreNPCangle']].find(
+        fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
+      ) || ['client', 'fStoreNPCangle'],
+    ),
+  ) || 0;
 
-  const currValue = item.getIn(
-    [['server', 'nStoreTrade'], ['client', 'nStoreTrade']].find(
-      fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
-    ) || ['server', 'nStoreTrade'],
-    0,
-  );
-
-  return !isNullOrUndefined(nextValue) ? nextValue : currValue;
-};
-
-export const getUseAngle = (nextValues, { item }) => {
-  const nextValue = nextValues && nextValues.getIn(['client', 'bSetNPCangle']);
-
-  const currValue = item.getIn(
-    [['client', 'bSetNPCangle']].find(
-      fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
-    ) || ['client', 'bSetNPCangle'],
-    false,
-  );
-
-  return !isNullOrUndefined(nextValue) ? nextValue : currValue;
-};
-
-export const getSize = (nextValues, { item }) => {
-  const nextValue = nextValues && nextValues.getIn(['client', 'fStoreNPCsize']);
-
-  const currValue = item.getIn(
-    [['client', 'fStoreNPCsize']].find(
-      fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
-    ) || ['client', 'fStoreNPCsize'],
-    1,
-  );
-
-  return !isNullOrUndefined(nextValue) ? nextValue : currValue;
-};
-
-export const getAngle = (nextValues, { item }) => {
-  const nextValue =
-    nextValues && nextValues.getIn(['client', 'fStoreNPCangle']);
-
-  const currValue = item.getIn(
-    [['client', 'fStoreNPCangle']].find(
-      fieldSets => !isNullOrUndefined(item.getIn(fieldSets)),
-    ) || ['client', 'fStoreNPCangle'],
-    0,
-  );
-
-  return !isNullOrUndefined(nextValue) ? nextValue : currValue;
-};
-
+/**
+ * Return vendor list item type from client cell
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ * @param {Object} props n: cell 1-200
+ *
+ * @returns Number
+ */
 export const getItemListClientType = (
   nextValues = IMMUTABLE_MAP,
   { item = IMMUTABLE_MAP },
   { n = 1 },
-) => {
-  const nextValue = nextValues.getIn(['client', `nItemListType__${n}_1`]);
-  const currValue = item.getIn(['client', `nItemListType__${n}_1`]);
-  return !isUndefined(nextValue) ? nextValue : currValue;
-};
+) =>
+  nextValues.getIn(
+    ['client', `nItemListType__${n}_1`],
+    item.getIn(['client', `nItemListType__${n}_1`]),
+  ) || 0;
 
+/**
+ * Return vendor list item code from client cell
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ * @param {Object} props n: cell 1-200
+ *
+ * @returns String
+ */
 export const getItemListClientCode = (
   nextValues = IMMUTABLE_MAP,
   { item = IMMUTABLE_MAP },
   { n = 1 },
-) => {
-  const nextValue = nextValues.getIn(['client', `strItemList__${n}_2`]);
-  const currValue = item.getIn(['client', `strItemList__${n}_2`], '');
-  return !isUndefined(nextValue) ? nextValue : currValue;
-};
+) =>
+  nextValues.getIn(
+    ['client', `strItemList__${n}_2`],
+    item.getIn(['client', `strItemList__${n}_2`]),
+  ) || '';
 
+/**
+ * Return vendor list item from client cell
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ * @param {Object} props n: cell 1-200
+ *
+ * @returns Immutable.Map|undefined
+ */
 export const getItemListClient = (
   nextValues = IMMUTABLE_MAP,
   { item = IMMUTABLE_MAP },
   { n = 1 },
-) => {
-  const nextValue = nextValues.getIn(['client', `itemList__${n}`]);
-  const currValue = item.getIn(['client', `itemList__${n}`]) || undefined;
-  return !isUndefined(nextValue) ? nextValue : currValue;
-};
+) =>
+  nextValues.getIn(
+    ['client', `itemList__${n}`],
+    item.getIn(['client', `itemList__${n}`]),
+  ) || undefined;
 
+/**
+ * Return vendor list item code from server cell
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ * @param {Object} props n: cell 1-200
+ *
+ * @returns String
+ */
 export const getItemListServerCode = (
   nextValues = IMMUTABLE_MAP,
   { item = IMMUTABLE_MAP },
   { n = 1 },
-) => {
-  const nextValue = nextValues.getIn(['server', `strItemCode__${n}`]);
-  const currValue = item.getIn(['server', `strItemCode__${n}`], '');
-  return !isUndefined(nextValue) ? nextValue : currValue;
-};
+) =>
+  nextValues.getIn(
+    ['server', `strItemCode__${n}`],
+    item.getIn(['server', `strItemCode__${n}`]),
+  ) || '';
 
+/**
+ * Return vendor list item all data
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ * @param {Object} props n: cell 1-200
+ *
+ * @returns Object
+ */
 export const getItemList = (...props) => ({
   client: getItemListClient(...props),
   clientType: getItemListClientType(...props),
@@ -137,7 +206,17 @@ export const getItemList = (...props) => ({
   n: props[2].n,
 });
 
-export const getItemsList = (nextValues, { item }) =>
+/**
+ * Return vendor list items all data
+ * @param {Object} nextValues next item values
+ * @param {Object} props item: the first thing we got from the server
+ *
+ * @returns Array
+ */
+export const getItemsList = (
+  nextValues = IMMUTABLE_MAP,
+  { item = IMMUTABLE_MAP },
+) =>
   Array.from(Array(200)).map((_, index) =>
     getItemList(nextValues, { item }, { n: index + 1 }),
   );
