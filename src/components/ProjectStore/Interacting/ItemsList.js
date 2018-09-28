177734e972e5c22ced6359ab3a941909034b0c87
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-28 05:05:23 +0300

    ProjectStoreInteractingItemsList

--- src/components/ProjectStore/Interacting/ItemsList.js
+++ src/components/ProjectStore/Interacting/ItemsList.js
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Map } from 'immutable';
+import { Map, List } from 'immutable';
 import SortableAutoSizeList, {
   CreateDragHangle,
   DragHangleDefault,
@@ -21,6 +21,24 @@ class ProjectStoreInteractingItemsList extends React.PureComponent {
   constructor(props) {
     super(props);
     this.renderItemList = this.renderItemList.bind(this);
+    this.sortableAutoSizeList = undefined;
+
+    /* eslint-disable */
+    this.createRef = sortableAutoSizeList =>
+      (this.sortableAutoSizeList = sortableAutoSizeList);
+    /* eslint-enable */
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { props } = this;
+
+    if (
+      this.sortableAutoSizeList &&
+      nextProps.projectNextValues &&
+      !nextProps.projectNextValues.equals(props.projectNextValues)
+    ) {
+      this.sortableAutoSizeList.forceUpdateGrid();
+    }
   }
 
   renderItemList(
@@ -29,7 +47,16 @@ class ProjectStoreInteractingItemsList extends React.PureComponent {
       virtualized: { isVisible, isScrolling },
     },
   ) {
-    const { item, itemNextValues } = this.props;
+    const {
+      item,
+      itemNextValues,
+      projectNextValues,
+      itemActions,
+      localSettings,
+      moneyTypes,
+      itemGrades,
+      actions,
+    } = this.props;
 
     if (!isVisible || isScrolling) {
       return null;
@@ -41,6 +68,12 @@ class ProjectStoreInteractingItemsList extends React.PureComponent {
         itemNextValues={itemNextValues}
         index={index}
         dragHandle={<DragHandle />}
+        projectNextValues={projectNextValues}
+        actions={actions}
+        itemActions={itemActions}
+        localSettings={localSettings}
+        moneyTypes={moneyTypes}
+        itemGrades={itemGrades}
       />
     );
   }
@@ -48,7 +81,8 @@ class ProjectStoreInteractingItemsList extends React.PureComponent {
   render() {
     return (
       <SortableAutoSizeList
-        rowHeight={55}
+        ref={this.createRef}
+        rowHeight={75}
         rowCount={200}
         rowRenderer={this.renderItemList}
         useDragHandle
@@ -60,6 +94,12 @@ class ProjectStoreInteractingItemsList extends React.PureComponent {
 ProjectStoreInteractingItemsList.propTypes = {
   item: PropTypes.instanceOf(Map).isRequired,
   itemNextValues: PropTypes.instanceOf(Map).isRequired,
+  localSettings: PropTypes.instanceOf(Map).isRequired,
+  projectNextValues: PropTypes.instanceOf(Map).isRequired,
+  actions: PropTypes.object.isRequired,
+  itemActions: PropTypes.object.isRequired,
+  moneyTypes: PropTypes.instanceOf(List).isRequired,
+  itemGrades: PropTypes.instanceOf(List).isRequired,
 };
 
 ProjectStoreInteractingItemsList.defaultProps = {};
