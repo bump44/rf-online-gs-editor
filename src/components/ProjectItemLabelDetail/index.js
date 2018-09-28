66e8c5de0f47cb95ba16ff2ba6839852c05ac0c7
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-29 02:00:04 +0300

    ProjectItemLabelDetail

--- src/components/ProjectItemLabelDetail/index.js
+++ src/components/ProjectItemLabelDetail/index.js
@@ -19,63 +19,81 @@ import {
   getType,
 } from '../../containers/App/getters/projectItem';
 
-function ProjectItemLabelDetail(props) {
-  const { item, itemNextValues, link, size } = props;
-
-  const nextValue = itemNextValues.get('nextValue');
-
-  const isSaved = itemNextValues.getIn(['isSaved'], true);
-  const isSaving = itemNextValues.getIn(['isSaving'], false);
-  const isError = itemNextValues.getIn(['isError'], false);
-  const errorMessage = itemNextValues.getIn(['errorMessage'], '');
-  const nIndex = getIndex(nextValue, { item });
-
-  const isShowStatus =
-    (!isSaving && !isSaved && !isError) || isSaving || isError;
-
-  const componentProps = {
-    title: isError ? errorMessage : undefined,
-    color: cx({
-      teal:
-        !isError &&
-        !(itemNextValues.getIn(['isSaved']) !== undefined && isSaved) &&
-        !isSaving &&
-        !(isShowStatus && !isSaving && !isSaved && !isError),
-      red: isError,
-      green: itemNextValues.getIn(['isSaved']) !== undefined && isSaved,
-      yellow: isSaving,
-      pink: isShowStatus && !isSaving && !isSaved && !isError,
-    }),
-    image: true,
-  };
-
-  if (link) {
-    const id = getId(nextValue, { item });
-    const projectId = getProjectId(nextValue, { item });
-
-    componentProps.as = Link;
-    componentProps.to = `/project/${projectId}/items/${id}`;
+/* eslint-disable react/prefer-stateless-function */
+class ProjectItemLabelDetail extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
   }
 
-  const type = getType(nextValue, { item });
+  onClick(evt) {
+    evt.preventDefault();
+    const { onClick, item, itemNextValues } = this.props;
+    onClick(item, itemNextValues);
+  }
 
-  return (
-    <Label {...componentProps} size={size}>
-      <Icon
-        loading={isShowStatus && isSaving}
-        name={cx({
-          check: !isShowStatus,
-          pencil: isShowStatus && !isSaving && !isSaved && !isError,
-          times: isShowStatus && isError,
-          spinner: isShowStatus && isSaving,
-        })}
-      />
-      {nIndex}
-      <Label.Detail>
-        <ProjectItemTypeLocaleMessage messageKey={type} tagName="small" />
-      </Label.Detail>
-    </Label>
-  );
+  render() {
+    const { item, itemNextValues, link, size, onClick } = this.props;
+
+    const nextValue = itemNextValues.get('nextValue');
+
+    const isSaved = itemNextValues.getIn(['isSaved'], true);
+    const isSaving = itemNextValues.getIn(['isSaving'], false);
+    const isError = itemNextValues.getIn(['isError'], false);
+    const errorMessage = itemNextValues.getIn(['errorMessage'], '');
+    const nIndex = getIndex(nextValue, { entry: item });
+
+    const isShowStatus =
+      (!isSaving && !isSaved && !isError) || isSaving || isError;
+
+    const componentProps = {
+      title: isError ? errorMessage : undefined,
+      color: cx({
+        teal:
+          !isError &&
+          !(itemNextValues.getIn(['isSaved']) !== undefined && isSaved) &&
+          !isSaving &&
+          !(isShowStatus && !isSaving && !isSaved && !isError),
+        red: isError,
+        green: itemNextValues.getIn(['isSaved']) !== undefined && isSaved,
+        yellow: isSaving,
+        pink: isShowStatus && !isSaving && !isSaved && !isError,
+      }),
+      image: true,
+    };
+
+    if (link || onClick) {
+      const id = getId(nextValue, { entry: item });
+      const projectId = getProjectId(nextValue, { entry: item });
+
+      componentProps.as = Link;
+      componentProps.to = `/project/${projectId}/items/${id}`;
+    }
+
+    if (onClick) {
+      componentProps.onClick = this.onClick;
+    }
+
+    const type = getType(nextValue, { entry: item });
+
+    return (
+      <Label {...componentProps} size={size}>
+        <Icon
+          loading={isShowStatus && isSaving}
+          name={cx({
+            check: !isShowStatus,
+            pencil: isShowStatus && !isSaving && !isSaved && !isError,
+            times: isShowStatus && isError,
+            spinner: isShowStatus && isSaving,
+          })}
+        />
+        {nIndex}
+        <Label.Detail>
+          <ProjectItemTypeLocaleMessage messageKey={type} tagName="small" />
+        </Label.Detail>
+      </Label>
+    );
+  }
 }
 
 ProjectItemLabelDetail.propTypes = {
@@ -83,11 +101,13 @@ ProjectItemLabelDetail.propTypes = {
   itemNextValues: PropTypes.instanceOf(Map).isRequired,
   link: PropTypes.bool,
   size: PropTypes.oneOf(['mini', 'tiny', 'small', 'large', 'huge']),
+  onClick: PropTypes.func,
 };
 
 ProjectItemLabelDetail.defaultProps = {
   link: false,
   size: 'small',
+  onClick: undefined,
 };
 
 export default ProjectItemLabelDetail;
