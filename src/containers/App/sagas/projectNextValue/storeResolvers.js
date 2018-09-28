7960df709ed584a2aa7ac099a5b31450dcdc044e
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-28 17:08:07 +0300

    update resolvers

--- src/containers/App/sagas/projectNextValue/storeResolvers.js
+++ src/containers/App/sagas/projectNextValue/storeResolvers.js
@@ -1,3 +1,5 @@
+import { getItemList } from '../../getters/projectStore';
+
 const Resolvers = {
   name: (item, nextValue) =>
     item
@@ -23,11 +25,66 @@ const Resolvers = {
 
   itemListRemove: (item, n) =>
     item
-      .setIn(['server', `strItemCode__${n}`], '0')
-      .setIn(['client', `strItemList__${n}_2`], '00000000')
+      .setIn(['server', `strItemCode__${n}`], '')
+      .setIn(['client', `strItemList__${n}_2`], '')
       .setIn(['client', `nItemListType__${n}_1`], 0)
       .setIn(['client', `itemList__${n}`], null)
       .setIn(['server', `itemList__${n}`], null),
+
+  itemListUpdate: (
+    item,
+    { n, clientCode = '', clientType = 0, serverCode = '', itemList } = {},
+  ) =>
+    item
+      .setIn(['server', `strItemCode__${n}`], serverCode)
+      .setIn(['client', `strItemList__${n}_2`], clientCode)
+      .setIn(['client', `nItemListType__${n}_1`], clientType)
+      .setIn(['client', `itemList__${n}`], itemList)
+      .setIn(['server', `itemList__${n}`], itemList),
+
+  itemsListReshuffle: (nextValue, value, { item }) => {
+    const itemsList = [];
+
+    let setToN = 1;
+    let nextItemValues = nextValue;
+
+    Array.from(Array(200)).forEach((_, index) => {
+      const n = index + 1;
+      const {
+        serverCode,
+        clientCode,
+        clientType,
+        client,
+        server,
+      } = getItemList(nextItemValues, { item }, { n });
+
+      if (serverCode && serverCode.length > 1) {
+        itemsList.push({ serverCode, clientCode, clientType });
+
+        nextItemValues = Resolvers.itemListUpdate(nextItemValues, {
+          n: setToN,
+          serverCode,
+          clientCode,
+          clientType,
+          itemList: server || client,
+        });
+
+        setToN += 1;
+      }
+    });
+
+    Array.from(Array(200 - setToN - 1)).forEach((_, index) => {
+      const n = index + setToN;
+      nextItemValues = Resolvers.itemListRemove(nextItemValues, n);
+    });
+
+    return nextItemValues;
+  },
+
+  itemsListCount: (item, listCount) =>
+    item
+      .setIn(['server', `nStoreListCount`], listCount)
+      .setIn(['client', `nStoreLISTcount`], listCount),
 };
 
 export default Resolvers;
