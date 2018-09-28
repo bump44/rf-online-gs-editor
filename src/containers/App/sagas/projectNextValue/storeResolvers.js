bd2abe8f6ae0ff48fa4b92a86fa1da08a8f97cae
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-28 05:06:36 +0300

    itemListRemove

--- src/containers/App/sagas/projectNextValue/storeResolvers.js
+++ src/containers/App/sagas/projectNextValue/storeResolvers.js
@@ -20,6 +20,14 @@ const Resolvers = {
 
   angle: (item, nextValue) =>
     item.setIn(['client', 'fStoreNPCangle'], nextValue),
+
+  itemListRemove: (item, n) =>
+    item
+      .setIn(['server', `strItemCode__${n}`], '0')
+      .setIn(['client', `strItemList__${n}_2`], '00000000')
+      .setIn(['client', `nItemListType__${n}_1`], 0)
+      .setIn(['client', `itemList__${n}`], null)
+      .setIn(['server', `itemList__${n}`], null),
 };
 
 export default Resolvers;
