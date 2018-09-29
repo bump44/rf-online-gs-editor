f817b1c78d32ace28d6cb3c39129368dce79fe81
Ilya Gerasimov <r0sky1@yandex.ru>
2018-09-29 16:10:16 +0300

    ProjectItemRenderTool

--- src/components/ProjectItem/Render/Tool.js
+++ src/components/ProjectItem/Render/Tool.js
@@ -7,31 +7,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Map, List } from 'immutable';
-import { Grid, Header, Segment } from 'semantic-ui-react';
-// import styled from 'styled-components';
-
-// import { FormattedMessage } from 'react-intl';
-// import messages from '../messages';
-
-import ProjectItemInteractingName from '../Interacting/Name';
-import ProjectItemInteractingExchange from '../Interacting/Exchange';
-import ProjectItemInteractingSell from '../Interacting/Sell';
-import ProjectItemInteractingGround from '../Interacting/Ground';
-import ProjectItemInteractingStoragePossible from '../Interacting/StoragePossible';
-import ProjectItemInteractingMoneyType from '../Interacting/MoneyType';
-import ProjectItemInteractingMoneyValue from '../Interacting/MoneyValue';
-import ProjectItemInteractingStoragePrice from '../Interacting/StoragePrice';
-import ProjectItemInteractingCivilBM from '../Interacting/CivilBM';
-import ProjectItemInteractingCivilBF from '../Interacting/CivilBF';
-import ProjectItemInteractingCivilCM from '../Interacting/CivilCM';
-import ProjectItemInteractingCivilCF from '../Interacting/CivilCF';
-import ProjectItemInteractingCivilA from '../Interacting/CivilA';
-import ProjectItemInteractingStdPrice from '../Interacting/StdPrice';
-import ProjectItemInteractingStdPoint from '../Interacting/StdPoint';
-import ProjectItemInteractingGoldPoint from '../Interacting/GoldPoint';
-import ProjectItemInteractingProcPoint from '../Interacting/ProcPoint';
-import ProjectItemInteractingKillPoint from '../Interacting/KillPoint';
-import ProjectItemInteractingItemGrade from '../Interacting/ItemGrade';
+import SegmentBasic from '../Segment/Basic';
 
 /* eslint-disable react/prefer-stateless-function */
 class ProjectItemRenderTool extends React.PureComponent {
@@ -45,152 +21,15 @@ class ProjectItemRenderTool extends React.PureComponent {
     } = this.props;
 
     return (
-      <Segment>
-        <Grid>
-          <Grid.Row columns={2}>
-            <Grid.Column width={1} verticalAlign="top">
-              <Header sub>Name</Header>
-              <Header sub>Transfer & Civil permissions</Header>
-            </Grid.Column>
-            <Grid.Column width={6} verticalAlign="top">
-              <ProjectItemInteractingName
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeName}
-                className="mb-15"
-                label={
-                  <ProjectItemInteractingItemGrade
-                    item={item}
-                    itemNextValues={itemNextValues}
-                    onChangeValue={itemActions.changeItemGrade}
-                    className="mr-15 mb-5"
-                    types={itemGrades}
-                  />
-                }
-              />
-              <ProjectItemInteractingExchange
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeExchange}
-                className="mr-15 mb-5"
-              />
-              <ProjectItemInteractingSell
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeSell}
-                className="mr-15 mb-5"
-              />
-              <ProjectItemInteractingGround
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeGround}
-                className="mr-15 mb-5"
-              />
-              <ProjectItemInteractingStoragePossible
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStoragePossible}
-                className="mr-15 mb-5"
-              />
-              <div>
-                <ProjectItemInteractingCivilBM
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilBM}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilBF
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilBF}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilCM
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilCM}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilCF
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilCF}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilA
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilA}
-                  className="mr-15 mb-5"
-                />
-              </div>
-            </Grid.Column>
-            <Grid.Column width={9} verticalAlign="top">
-              <ProjectItemInteractingMoneyValue
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeStdPrice={itemActions.changeStdPrice}
-                onChangeStdPoint={itemActions.changeStdPoint}
-                onChangeGoldPoint={itemActions.changeGoldPoint}
-                onChangeProcPoint={itemActions.changeProcPoint}
-                onChangeKillPoint={itemActions.changeKillPoint}
-                types={moneyTypes}
-                className="mb-5"
-                label={
-                  <ProjectItemInteractingMoneyType
-                    item={item}
-                    itemNextValues={itemNextValues}
-                    onChangeValue={itemActions.changeMoney}
-                    types={moneyTypes}
-                  />
-                }
-              />
-              <ProjectItemInteractingStdPrice
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStdPrice}
-                label="StdPrice"
-                className="mb-5"
-              />
-              <ProjectItemInteractingStdPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStdPoint}
-                label="StdPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingGoldPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeGoldPoint}
-                label="GoldPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingProcPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeProcPoint}
-                label="ProcPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingKillPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeKillPoint}
-                label="KillPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingStoragePrice
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStoragePrice}
-                types={moneyTypes}
-                label="Storage price"
-              />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <React.Fragment>
+        <SegmentBasic
+          item={item}
+          itemNextValues={itemNextValues}
+          itemActions={itemActions}
+          moneyTypes={moneyTypes}
+          itemGrades={itemGrades}
+        />
+      </React.Fragment>
     );
   }
 }
