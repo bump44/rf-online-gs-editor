f1d4cc5d8b5bf043aa542027b1812d534b07b532
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-01 06:46:49 +0300

    ProjectBoxItemOutLabelDetail

---
+++ src/components/ProjectBoxItemOutLabelDetail/index.js
@@ -0,0 +1,93 @@
+/**
+ *
+ * ProjectBoxItemOutLabelDetail
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { Link } from 'react-router-dom';
+import { Map } from 'immutable';
+import { Label, Icon } from 'semantic-ui-react';
+import { getIndex } from '../../containers/App/getters/projectBoxItemOut';
+
+/* eslint-disable react/prefer-stateless-function */
+class ProjectBoxItemOutLabelDetail extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(evt) {
+    evt.preventDefault();
+    const { onClick, boxItemOut, boxItemOutNextValues } = this.props;
+    onClick(boxItemOut, boxItemOutNextValues);
+  }
+
+  render() {
+    const { boxItemOut, boxItemOutNextValues, size, onClick } = this.props;
+
+    const nextValue = boxItemOutNextValues.get('nextValue');
+
+    const isSaved = boxItemOutNextValues.getIn(['isSaved'], true);
+    const isSaving = boxItemOutNextValues.getIn(['isSaving'], false);
+    const isError = boxItemOutNextValues.getIn(['isError'], false);
+    const errorMessage = boxItemOutNextValues.getIn(['errorMessage'], '');
+    const nIndex = getIndex(nextValue, { entry: boxItemOut });
+
+    const isShowStatus =
+      (!isSaving && !isSaved && !isError) || isSaving || isError;
+
+    const componentProps = {
+      title: isError ? errorMessage : undefined,
+      color: cx({
+        teal:
+          !isError &&
+          !(boxItemOutNextValues.getIn(['isSaved']) !== undefined && isSaved) &&
+          !isSaving &&
+          !(isShowStatus && !isSaving && !isSaved && !isError),
+        red: isError,
+        green: boxItemOutNextValues.getIn(['isSaved']) !== undefined && isSaved,
+        yellow: isSaving,
+        pink: isShowStatus && !isSaving && !isSaved && !isError,
+      }),
+      image: true,
+    };
+
+    if (onClick) {
+      componentProps.as = Link;
+      componentProps.to = `/`; // required
+      componentProps.onClick = this.onClick;
+    }
+
+    return (
+      <Label {...componentProps} size={size}>
+        <Icon
+          loading={isShowStatus && isSaving}
+          name={cx({
+            check: !isShowStatus,
+            pencil: isShowStatus && !isSaving && !isSaved && !isError,
+            times: isShowStatus && isError,
+            spinner: isShowStatus && isSaving,
+          })}
+        />
+        {nIndex}
+      </Label>
+    );
+  }
+}
+
+ProjectBoxItemOutLabelDetail.propTypes = {
+  boxItemOut: PropTypes.instanceOf(Map).isRequired,
+  boxItemOutNextValues: PropTypes.instanceOf(Map).isRequired,
+  size: PropTypes.oneOf(['mini', 'tiny', 'small', 'large', 'huge']),
+  onClick: PropTypes.func,
+};
+
+ProjectBoxItemOutLabelDetail.defaultProps = {
+  size: 'small',
+  onClick: undefined,
+};
+
+export default ProjectBoxItemOutLabelDetail;
---
+++ src/components/ProjectBoxItemOutLabelDetail/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ProjectBoxItemOutLabelDetail from '../index';
+
+describe('<ProjectBoxItemOutLabelDetail />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
