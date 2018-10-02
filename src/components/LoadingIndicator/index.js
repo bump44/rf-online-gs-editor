85395e4d78cd3de06695f38859e31e1713270392
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-02 10:07:02 +0300

    ms

--- src/components/LoadingIndicator/index.js
+++ src/components/LoadingIndicator/index.js
@@ -9,8 +9,34 @@ import React from 'react';
 // import styled from 'styled-components';
 import { Loader } from 'semantic-ui-react';
 
-function LoadingIndicator() {
-  return <Loader active inline="centered" />;
+class LoadingIndicator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { ms: 0 };
+    this.interval = undefined;
+  }
+
+  componentWillMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        ms: prevState.ms + 1,
+      }));
+    }, 1);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { ms } = this.state;
+
+    return (
+      <Loader active inline="centered">
+        {ms}
+      </Loader>
+    );
+  }
 }
 
 LoadingIndicator.propTypes = {};
