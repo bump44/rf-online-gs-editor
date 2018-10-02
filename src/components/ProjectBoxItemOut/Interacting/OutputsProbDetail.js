1cd0b47cad4e25603ccf3fc48bb72bf761816731
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-02 10:07:34 +0300

    ProjectBoxItemOutInteractingOutputsProbDetail

---
+++ src/components/ProjectBoxItemOut/Interacting/OutputsProbDetail.js
@@ -0,0 +1,61 @@
+/**
+ *
+ * ProjectBoxItemOutInteractingOutputsProbDetail
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Map } from 'immutable';
+import { Label } from 'semantic-ui-react';
+import { FormattedMessage } from 'react-intl';
+import messages from '../messages';
+
+import * as projectBoxItemOut from '../../../containers/App/getters/projectBoxItemOut';
+
+/* eslint-disable react/prefer-stateless-function */
+class ProjectBoxItemOutInteractingOutputsProbDetail extends React.PureComponent {
+  render() {
+    const { boxItemOutNextValues, boxItemOut } = this.props;
+    const outputsProb = projectBoxItemOut.getOutputsProb(
+      boxItemOutNextValues.get('nextValue'),
+      {
+        entry: boxItemOut,
+      },
+    );
+
+    const isCorrect = outputsProb === 10000;
+    const expected = outputsProb - 10000;
+
+    return (
+      <React.Fragment>
+        <Label color="grey">
+          <FormattedMessage {...messages.TotalProbability} />:
+        </Label>
+        {isCorrect && (
+          <Label color="green">
+            <FormattedMessage {...messages.isCorrect} />
+          </Label>
+        )}
+        {!isCorrect && (
+          <Label color="red">
+            <FormattedMessage {...messages.incorrect} />
+            :&nbsp;
+            {outputsProb}
+            {expected < 0 ? '+' : '-'}
+            <i>{expected.toString().replace(/[^\d]/g, '')}</i>
+          </Label>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+ProjectBoxItemOutInteractingOutputsProbDetail.propTypes = {
+  boxItemOut: PropTypes.instanceOf(Map).isRequired,
+  boxItemOutNextValues: PropTypes.instanceOf(Map).isRequired,
+};
+
+ProjectBoxItemOutInteractingOutputsProbDetail.defaultProps = {};
+
+export default ProjectBoxItemOutInteractingOutputsProbDetail;
