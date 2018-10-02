74c990166dde1c10cfdae145465b1cb367b95194
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-02 06:01:27 +0300

    ProjectBoxItemOut

---
+++ src/components/ProjectBoxItemOut/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ProjectBoxItemOut from '../index';
+
+describe('<ProjectBoxItemOut />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
