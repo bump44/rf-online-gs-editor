4932b62c74bad3f183b60f85c793a05bcf21f57b
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-02 10:09:24 +0300

    render outputs

--- src/components/ProjectItem/Render/Box.js
+++ src/components/ProjectItem/Render/Box.js
@@ -7,8 +7,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Map, List } from 'immutable';
-import { Tab } from 'semantic-ui-react';
+import { Tab, Segment } from 'semantic-ui-react';
+import { IMMUTABLE_MAP } from '../../../containers/App/constants';
 import SegmentBasic from '../Segment/Basic';
+import * as projectItem from '../../../containers/App/getters/projectItem';
+import * as projectBoxItemOut from '../../../containers/App/getters/projectBoxItemOut';
+
+import ProjectBoxItemOutLabelDetail from '../../ProjectBoxItemOutLabelDetail';
+import ProjectBoxItemOutInteractingOutputsProbDetail from '../../ProjectBoxItemOut/Interacting/OutputsProbDetail';
+import ProjectBoxItemOutInteractingOutputs from '../../ProjectBoxItemOut/Interacting/Outputs';
 
 const tabStyle = { height: '100%' };
 const tabMenu = {
@@ -47,7 +54,61 @@ class ProjectItemRenderBox extends React.PureComponent {
   }
 
   renderOutputs() {
-    return <div>renderOutputs</div>;
+    const {
+      item,
+      itemNextValues,
+      nextValues,
+      boxItemOutActions,
+      entriesFinderItemsActions,
+      entriesFinderItems,
+      itemActions,
+      moneyTypes,
+      itemGrades,
+      weaponTypes,
+      localSettings,
+    } = this.props;
+
+    const boxItemOut = projectItem.getBoxItemOut(
+      itemNextValues.get('nextValue'),
+      { entry: item },
+    );
+
+    if (!boxItemOut) {
+      return <div>Outputs not defined</div>;
+    }
+
+    const boxItemOutNextValues = nextValues.get(
+      projectBoxItemOut.getId(undefined, { entry: boxItemOut }),
+      IMMUTABLE_MAP,
+    );
+
+    return (
+      <Segment>
+        <ProjectBoxItemOutLabelDetail
+          boxItemOut={boxItemOut}
+          boxItemOutNextValues={boxItemOutNextValues}
+        />
+
+        <ProjectBoxItemOutInteractingOutputsProbDetail
+          boxItemOut={boxItemOut}
+          boxItemOutNextValues={boxItemOutNextValues}
+        />
+
+        <ProjectBoxItemOutInteractingOutputs
+          boxItemOut={boxItemOut}
+          boxItemOutNextValues={boxItemOutNextValues}
+          boxItemOutActions={boxItemOutActions}
+          nextValues={nextValues}
+          entriesFinderItemsActions={entriesFinderItemsActions}
+          entriesFinderItems={entriesFinderItems}
+          itemActions={itemActions}
+          moneyTypes={moneyTypes}
+          itemGrades={itemGrades}
+          weaponTypes={weaponTypes}
+          localSettings={localSettings}
+        />
+      </Segment>
+    );
   }
 
   renderBasic() {
@@ -82,9 +143,15 @@ class ProjectItemRenderBox extends React.PureComponent {
 ProjectItemRenderBox.propTypes = {
   item: PropTypes.instanceOf(Map).isRequired,
   itemNextValues: PropTypes.instanceOf(Map).isRequired,
+  nextValues: PropTypes.instanceOf(Map).isRequired,
   moneyTypes: PropTypes.instanceOf(List).isRequired,
   itemGrades: PropTypes.instanceOf(List).isRequired,
+  weaponTypes: PropTypes.instanceOf(List).isRequired,
+  localSettings: PropTypes.instanceOf(Map).isRequired,
   itemActions: PropTypes.object.isRequired,
+  boxItemOutActions: PropTypes.object.isRequired,
+  entriesFinderItemsActions: PropTypes.object.isRequired,
+  entriesFinderItems: PropTypes.instanceOf(Map).isRequired,
 };
 
 export default ProjectItemRenderBox;
