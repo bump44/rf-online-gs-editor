2e5688538b7bc0b8caa553fc6153946f973eb24d
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-02 06:01:02 +0300

    ProjectBoxItemOut

---
+++ src/components/ProjectBoxItemOut/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ProjectBoxItemOut
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function ProjectBoxItemOut() {
+  return <div />;
+}
+
+ProjectBoxItemOut.propTypes = {};
+
+export default ProjectBoxItemOut;
---
+++ src/components/ProjectBoxItemOut/messages.js
@@ -0,0 +1,14 @@
+/*
+ * ProjectBoxItemOut Messages
+ *
+ * This contains all the text for the ProjectBoxItemOut component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  Disable: {
+    id: 'app.components.ProjectBoxItemOut.Disable',
+    defaultMessage: 'Disable',
+  },
+});
