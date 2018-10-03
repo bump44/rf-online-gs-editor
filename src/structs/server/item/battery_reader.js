885e1d0eaeb43fc61e30463adb7995208139a7db
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-03 16:21:08 +0300

    add battery reader

---
+++ src/structs/server/item/battery_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { BATTERY } from '../../item_types';
+import defaultHeader from './default_header';
+import boxStruct from './box_struct';
+
+export default class ServerItemBatteryReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemBatteryReader',
+      struct,
+    });
+  }
+}
+
+const struct = [
+  {
+    type: BATTERY,
+    header: defaultHeader,
+    block: boxStruct,
+  },
+];
