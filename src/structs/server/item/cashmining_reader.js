2da3a00afabb736c7a069ffaa4924209d5100b5e
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:04:58 +0300

    cashmining reader & struct

---
+++ src/structs/server/item/cashmining_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { CASHMINING } from '../../item_types';
+import defaultHeader from './default_header';
+import cashminingStruct from './cashmining_struct';
+
+export default class ServerItemCashminingReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemCashminingReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: CASHMINING,
+    header: defaultHeader,
+    block: cashminingStruct,
+  },
+];
