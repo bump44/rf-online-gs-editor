9463d86c5f8c4b3d91dc894b73a0356bed71641d
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:08:21 +0300

    tower reader & struct

---
+++ src/structs/server/item/tower_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { TOWER } from '../../item_types';
+import defaultHeader from './default_header';
+import towerStruct from './tower_struct';
+
+export default class ServerItemTowerReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemTowerReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: TOWER,
+    header: defaultHeader,
+    block: towerStruct,
+  },
+];
