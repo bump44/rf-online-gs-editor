c808f9d4865f7e5a13f66349cfa7dca058fa413f
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:08:30 +0300

    town reader & struct

---
+++ src/structs/server/item/town_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { TOWN } from '../../item_types';
+import defaultHeader from './default_header';
+import townStruct from './town_struct';
+
+export default class ServerItemTownReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemTownReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: TOWN,
+    header: defaultHeader,
+    block: townStruct,
+  },
+];
