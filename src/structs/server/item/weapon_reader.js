0b81e86921e8cc6ee5b71024fbcdcd8228b628bf
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-10 15:27:37 +0300

    weapon reader & struct

---
+++ src/structs/server/item/weapon_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { WEAPON } from '../../item_types';
+import defaultHeader from './default_header';
+import weaponStruct from './weapon_struct';
+
+export default class ServerItemWeaponReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemWeaponReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: WEAPON,
+    header: defaultHeader,
+    block: weaponStruct,
+  },
+];
