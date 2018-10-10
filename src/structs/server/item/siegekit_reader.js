2e9063a22cce758661a6eae4e728c3944c2c96d2
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:07:56 +0300

    siegekit reader & struct

---
+++ src/structs/server/item/siegekit_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { SIEGEKIT } from '../../item_types';
+import defaultHeader from './default_header';
+import siegekitStruct from './siegekit_struct';
+
+export default class ServerItemSiegekitReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemSiegekitReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: SIEGEKIT,
+    header: defaultHeader,
+    block: siegekitStruct,
+  },
+];
