375376cdfa6f8261b44052f6b550926f4fa0ce92
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:06:40 +0300

    potion reader & struct

---
+++ src/structs/server/item/potion_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { POTION } from '../../item_types';
+import defaultHeader from './default_header';
+import potionStruct from './potion_struct';
+
+export default class ServerItemPotionReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemPotionReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: POTION,
+    header: defaultHeader,
+    block: potionStruct,
+  },
+];
