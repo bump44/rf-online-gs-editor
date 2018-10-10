c0eaf7322da35a0a1ee3895c5af686216bea09d2
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:05:08 +0300

    booty reader & struct

---
+++ src/structs/server/item/booty_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { BOOTY } from '../../item_types';
+import defaultHeader from './default_header';
+import bootyStruct from './booty_struct';
+
+export default class ServerItemBootyReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemBootyReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: BOOTY,
+    header: defaultHeader,
+    block: bootyStruct,
+  },
+];
