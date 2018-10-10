90780de30483c7ab1ec93fb5200dbdfdd70c71b8
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:05:51 +0300

    force reader & struct

---
+++ src/structs/server/item/force_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { FORCE } from '../../item_types';
+import defaultHeader from './default_header';
+import forceStruct from './force_struct';
+
+export default class ServerItemForceReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemForceReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: FORCE,
+    header: defaultHeader,
+    block: forceStruct,
+  },
+];
