c6099e2cfcdfda77a1db72a1f1f54f8c81e221a7
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:08:40 +0300

    trap reader & struct

---
+++ src/structs/server/item/trap_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { TRAP } from '../../item_types';
+import defaultHeader from './default_header';
+import trapStruct from './trap_struct';
+
+export default class ServerItemTrapReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemTrapReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: TRAP,
+    header: defaultHeader,
+    block: trapStruct,
+  },
+];
