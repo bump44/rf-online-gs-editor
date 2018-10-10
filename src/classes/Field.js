38c279e78b833b13b190fad29a5e65da8822f883
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-10 15:27:02 +0300

    invariant

--- src/classes/Field.js
+++ src/classes/Field.js
@@ -1,50 +1,102 @@
 /* eslint-disable default-case, no-fallthrough */
-import { get } from 'lodash';
+import invariant from 'invariant';
+import { isInteger } from 'lodash';
 
 class Field {
-  constructor({ type, name, len, as, schemaProps, writerProps }) {
+  constructor({ type, name, len, as, schemaProps = {} } = {}) {
+    invariant(
+      [Number, Boolean, String].includes(type),
+      `Field type must be one of Number|Boolean|String`,
+    );
+
+    invariant(
+      typeof name === 'string' && name.length >= 2,
+      `Field name must be a string of at least two characters`,
+    );
+
+    invariant(
+      typeof len === 'function' || isInteger(len),
+      `Field len must be a integer or function`,
+    );
+
+    invariant(
+      !as ||
+        (as === 'hex' && type === String) ||
+        (as === 'float' && type === Number),
+      `Field as must be undefined or type: String & as: 'hex'|type: Number & as: 'float'`,
+    );
+
     this.type = type;
     this.name = name;
     this.len = len;
     this.as = as;
-    this.schemaProps = schemaProps || {};
-    this.writerProps = writerProps || {};
+    this.schemaProps = schemaProps;
     this.dynamic = typeof this.len === 'function';
   }
 
+  /**
+   * Is have dynamic attribute
+   *
+   * @returns Boolean
+   */
   isDynamic() {
     return this.dynamic;
   }
 
+  /**
+   * Return as attribute
+   *
+   * @returns undefined|String oneOf(['hex', 'float'])
+   */
   getAs() {
     return this.as;
   }
 
+  /**
+   * Return name attribute
+   *
+   * @returns String
+   */
   getName() {
     return this.name;
   }
 
+  /**
+   * Return type attribute
+   *
+   * @returns Object oneOf([Number, Boolean, String])
+   */
   getType() {
     return this.type;
   }
 
-  getLen(values = {}) {
+  /**
+   * Return length attribute
+   * @param {Object} payload required if isDynamic true
+   *
+   * @returns Number
+   */
+  getLen(payload) {
+    invariant(!this.isDynamic() || payload, `Values is required`);
+    const values = payload || {};
     return typeof this.len === 'function' ? this.len(values) : this.len;
   }
 
-  getWriterProps() {
-    return this.writerProps;
-  }
+  /**
+   * Return field weight
+   * @param {Object} payload required if isDynamic true
+   *
+   * @returns Number
+   */
+  getWeight(payload) {
+    invariant(!this.isDynamic() || payload, `Values is required`);
+    const values = payload || {};
+    const len = this.getLen(values);
 
-  getWriterProp(key, returnDefault = null) {
-    return get(this.getWriterProps(), key, returnDefault);
-  }
-
-  getWeight(values = {}) {
     switch (this.getType()) {
       case Boolean:
       case Number:
-        switch (this.getLen(values)) {
+        switch (len) {
           case 8:
             return 1;
           case 16:
@@ -53,24 +105,32 @@ class Field {
             return 4;
           case 64:
             return 8;
+          default:
+            throw new Error(
+              `Unknown argument 'length' (${len}) on field type Boolean|Number, must be one of 8|16|32|64`,
+            );
         }
       case String:
         switch (this.getAs()) {
           case 'hex':
-            switch (this.getLen(values)) {
+            switch (len) {
               case 8:
                 return 1;
               case 16:
                 return 2;
               case 32:
                 return 4;
+              default:
+                throw new Error(
+                  `Unknown argument 'length' (${len}) on field type String & as: 'hex', must be one of 8|16|32`,
+                );
             }
           default:
-            return this.getLen(values);
+            return len;
         }
     }
 
-    throw new Error(`Undefined field type ${this.getType()}: ${this.getLen()}`);
+    throw new Error(`Unknown weight`);
   }
 
   toObject() {
