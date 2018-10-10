26ff61974375543e9f2a685b66739e0c86175dc2
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:05:19 +0300

    coupon reader & struct

---
+++ src/structs/server/item/coupon_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { COUPON } from '../../item_types';
+import defaultHeader from './default_header';
+import couponStruct from './coupon_struct';
+
+export default class ServerItemCouponReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemCouponReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: COUPON,
+    header: defaultHeader,
+    block: couponStruct,
+  },
+];
