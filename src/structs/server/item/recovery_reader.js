a0ef0337ad14ca64016474970d2de73f3ea5fa31
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:07:35 +0300

    recovery reader & struct

---
+++ src/structs/server/item/recovery_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { RECOVERY } from '../../item_types';
+import defaultHeader from './default_header';
+import recoveryStruct from './recovery_struct';
+
+export default class ServerItemRecoveryReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemRecoveryReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: RECOVERY,
+    header: defaultHeader,
+    block: recoveryStruct,
+  },
+];
