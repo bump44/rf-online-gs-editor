4b932d647832981ba71ac6c83ce5c08e3161ded1
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-10 15:27:22 +0300

    releaseFiles path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,8 @@ export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
 export const TMP_FOLDER = 'temporary';
+export const RELEASE_FILES_FOLDER = 'releaseFiles';
+
+export const RELEASE_FILES_CLIENTDAT_FOLDER = 'clientdat';
+export const RELEASE_FILES_CLIENT_FOLDER = 'client';
+export const RELEASE_FILES_SERVER_FOLDER = 'server';
--- src/utils/path.js
+++ src/utils/path.js
@@ -1,12 +1,15 @@
 import path from 'path';
+import { compact } from 'lodash';
 import { words } from './random';
-import { TMP_FOLDER } from './constants';
+import { TMP_FOLDER, RELEASE_FILES_FOLDER } from './constants';
 
 const getTmpPath = () => path.resolve('./', TMP_FOLDER);
+const getReleaseFilesPath = (...subfolders) =>
+  path.resolve('./', ...compact([RELEASE_FILES_FOLDER, ...subfolders]));
 
 const getRandomTmpPath = () => {
   const folders = [words(6), words(6), words(6)];
   return path.resolve(getTmpPath(), folders.join('/'));
 };
 
-export { getTmpPath, getRandomTmpPath };
+export { getTmpPath, getReleaseFilesPath, getRandomTmpPath };
