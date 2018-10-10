d6890839cda1c8537b22c52892949073c9f72545
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-11 00:05:40 +0300

    firecracker reader & struct

---
+++ src/structs/server/item/firecracker_reader.js
@@ -0,0 +1,22 @@
+import FileReader from '../../../classes/FileReader';
+import { FIRECRACKER } from '../../item_types';
+import defaultHeader from './default_header';
+import firecrackerStruct from './firecracker_struct';
+
+export default class ServerItemFirecrackerReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerItemFirecrackerReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    type: FIRECRACKER,
+    header: defaultHeader,
+    block: firecrackerStruct,
+  },
+];
