6760c8c5c1b35ff353dac69639b12313cba6b6ca
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-12 17:40:24 +0300

    basic armor segment

--- src/components/ProjectItem/Segment/BasicArmor.js
+++ src/components/ProjectItem/Segment/BasicArmor.js
@@ -28,161 +28,273 @@ import ProjectItemInteractingGoldPoint from '../Interacting/GoldPoint';
 import ProjectItemInteractingProcPoint from '../Interacting/ProcPoint';
 import ProjectItemInteractingKillPoint from '../Interacting/KillPoint';
 import ProjectItemInteractingItemGrade from '../Interacting/ItemGrade';
+import ProjectItemInteractingLevelLim from '../Interacting/LevelLim';
+import ProjectItemInteractingUpLevelLim from '../Interacting/UpLevelLim';
+import ProjectItemInteractingDefence from '../Interacting/Defence';
+import ProjectItemInteractingDefenceGap from '../Interacting/DefenceGap';
+import ProjectItemInteractingDefenceFacing from '../Interacting/DefenceFacing';
+import ProjectItemInteractingDefenceFacingPresent from '../Interacting/DefenceFacingPresent';
 
 /* eslint-disable react/prefer-stateless-function */
 class ProjectItemSegmentBasicArmor extends React.PureComponent {
-  render() {
-    const {
-      item,
-      itemNextValues,
-      itemActions,
-      moneyTypes,
-      itemGradeTypes,
-    } = this.props;
+  constructor(props) {
+    super(props);
 
+    this.renderNames = this.renderNames.bind(this);
+    this.renderTransfers = this.renderTransfers.bind(this);
+    this.renderCivils = this.renderCivils.bind(this);
+    this.renderCurrentMoney = this.renderCurrentMoney.bind(this);
+    this.renderAllMoneyFields = this.renderAllMoneyFields.bind(this);
+    this.renderLevels = this.renderLevels.bind(this);
+    this.renderDefences = this.renderDefences.bind(this);
+  }
+
+  renderNames() {
+    const { item, itemNextValues, itemActions, itemGradeTypes } = this.props;
+    return (
+      <div>
+        <ProjectItemInteractingName
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeName}
+          className="mb-15"
+          label={
+            <ProjectItemInteractingItemGrade
+              item={item}
+              itemNextValues={itemNextValues}
+              onChangeValue={itemActions.changeItemGrade}
+              className="mr-15 mb-5"
+              types={itemGradeTypes}
+            />
+          }
+        />
+      </div>
+    );
+  }
+
+  renderTransfers() {
+    const { item, itemNextValues, itemActions } = this.props;
+    return (
+      <div>
+        <ProjectItemInteractingExchange
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeExchange}
+          className="mr-15 mb-5"
+        />
+        <ProjectItemInteractingSell
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeSell}
+          className="mr-15 mb-5"
+        />
+        <ProjectItemInteractingGround
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeGround}
+          className="mr-15 mb-5"
+        />
+        <ProjectItemInteractingStoragePossible
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeStoragePossible}
+          className="mr-15 mb-5"
+        />
+      </div>
+    );
+  }
+
+  renderCivils() {
+    const { item, itemNextValues, itemActions } = this.props;
+    return (
+      <div>
+        <ProjectItemInteractingCivilBM
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeCivilBM}
+          className="mr-15 mb-5"
+        />
+        <ProjectItemInteractingCivilBF
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeCivilBF}
+          className="mr-15 mb-5"
+        />
+        <ProjectItemInteractingCivilCM
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeCivilCM}
+          className="mr-15 mb-5"
+        />
+        <ProjectItemInteractingCivilCF
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeCivilCF}
+          className="mr-15 mb-5"
+        />
+        <ProjectItemInteractingCivilA
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeCivilA}
+          className="mr-15 mb-5"
+        />
+      </div>
+    );
+  }
+
+  renderCurrentMoney() {
+    const { item, itemNextValues, itemActions, moneyTypes } = this.props;
+    return (
+      <div>
+        <ProjectItemInteractingMoneyValue
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeStdPrice={itemActions.changeStdPrice}
+          onChangeStdPoint={itemActions.changeStdPoint}
+          onChangeGoldPoint={itemActions.changeGoldPoint}
+          onChangeProcPoint={itemActions.changeProcPoint}
+          onChangeKillPoint={itemActions.changeKillPoint}
+          types={moneyTypes}
+          className="mb-5"
+          label={
+            <ProjectItemInteractingMoneyType
+              item={item}
+              itemNextValues={itemNextValues}
+              onChangeValue={itemActions.changeMoney}
+              types={moneyTypes}
+            />
+          }
+        />
+      </div>
+    );
+  }
+
+  renderAllMoneyFields() {
+    const { item, itemNextValues, itemActions, moneyTypes } = this.props;
+    return (
+      <div>
+        <ProjectItemInteractingStdPrice
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeStdPrice}
+          label="StdPrice"
+          className="mb-5"
+        />
+        <ProjectItemInteractingStdPoint
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeStdPoint}
+          label="StdPoint"
+          className="mb-5"
+        />
+        <ProjectItemInteractingGoldPoint
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeGoldPoint}
+          label="GoldPoint"
+          className="mb-5"
+        />
+        <ProjectItemInteractingProcPoint
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeProcPoint}
+          label="ProcPoint"
+          className="mb-5"
+        />
+        <ProjectItemInteractingKillPoint
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeKillPoint}
+          label="KillPoint"
+          className="mb-5"
+        />
+        <ProjectItemInteractingStoragePrice
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeStoragePrice}
+          types={moneyTypes}
+          label="Storage price"
+        />
+      </div>
+    );
+  }
+
+  renderLevels() {
+    const { item, itemNextValues, itemActions } = this.props;
+    return (
+      <div>
+        <ProjectItemInteractingLevelLim
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeLevelLim}
+          label="LevelLim"
+          className="mb-5"
+        />
+        <ProjectItemInteractingUpLevelLim
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeLevelLim}
+          label="UpLevelLim"
+        />
+      </div>
+    );
+  }
+
+  renderDefences() {
+    const { item, itemNextValues, itemActions } = this.props;
+    return (
+      <div>
+        <ProjectItemInteractingDefence
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeDefence}
+          className="mb-5"
+          label="Defence"
+        />
+        <ProjectItemInteractingDefenceGap
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeDefenceGap}
+          className="mb-5"
+          label="DefenceGap"
+        />
+        <ProjectItemInteractingDefenceFacing
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeDefenceFacing}
+          className="mb-5"
+          label="DefenceFacing"
+        />
+        <ProjectItemInteractingDefenceFacingPresent
+          item={item}
+          itemNextValues={itemNextValues}
+          onChangeValue={itemActions.changeDefenceFacing}
+          label="DefenceFacingPresent"
+        />
+      </div>
+    );
+  }
+
+  render() {
     return (
       <Segment>
         <Grid>
           <Grid.Row columns={2}>
-            <Grid.Column width={1} verticalAlign="top">
-              <Header sub>Name</Header>
-              <Header sub>Transfer & Civil permissions</Header>
-            </Grid.Column>
-            <Grid.Column width={6} verticalAlign="top">
-              <ProjectItemInteractingName
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeName}
-                className="mb-15"
-                label={
-                  <ProjectItemInteractingItemGrade
-                    item={item}
-                    itemNextValues={itemNextValues}
-                    onChangeValue={itemActions.changeItemGrade}
-                    className="mr-15 mb-5"
-                    types={itemGradeTypes}
-                  />
-                }
-              />
-              <ProjectItemInteractingExchange
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeExchange}
-                className="mr-15 mb-5"
-              />
-              <ProjectItemInteractingSell
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeSell}
-                className="mr-15 mb-5"
-              />
-              <ProjectItemInteractingGround
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeGround}
-                className="mr-15 mb-5"
-              />
-              <ProjectItemInteractingStoragePossible
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStoragePossible}
-                className="mr-15 mb-5"
-              />
-              <div>
-                <ProjectItemInteractingCivilBM
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilBM}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilBF
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilBF}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilCM
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilCM}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilCF
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilCF}
-                  className="mr-15 mb-5"
-                />
-                <ProjectItemInteractingCivilA
-                  item={item}
-                  itemNextValues={itemNextValues}
-                  onChangeValue={itemActions.changeCivilA}
-                  className="mr-15 mb-5"
-                />
-              </div>
+            <Grid.Column width={7} verticalAlign="top">
+              {this.renderNames()}
+              <Header size="tiny">Transfer & Civil permissions</Header>
+              {this.renderTransfers()}
+              {this.renderCivils()}
+              <Header size="tiny">Levels</Header>
+              {this.renderLevels()}
+              <Header size="tiny">Defences</Header>
+              {this.renderDefences()}
             </Grid.Column>
             <Grid.Column width={9} verticalAlign="top">
-              <ProjectItemInteractingMoneyValue
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeStdPrice={itemActions.changeStdPrice}
-                onChangeStdPoint={itemActions.changeStdPoint}
-                onChangeGoldPoint={itemActions.changeGoldPoint}
-                onChangeProcPoint={itemActions.changeProcPoint}
-                onChangeKillPoint={itemActions.changeKillPoint}
-                types={moneyTypes}
-                className="mb-5"
-                label={
-                  <ProjectItemInteractingMoneyType
-                    item={item}
-                    itemNextValues={itemNextValues}
-                    onChangeValue={itemActions.changeMoney}
-                    types={moneyTypes}
-                  />
-                }
-              />
-              <ProjectItemInteractingStdPrice
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStdPrice}
-                label="StdPrice"
-                className="mb-5"
-              />
-              <ProjectItemInteractingStdPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStdPoint}
-                label="StdPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingGoldPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeGoldPoint}
-                label="GoldPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingProcPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeProcPoint}
-                label="ProcPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingKillPoint
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeKillPoint}
-                label="KillPoint"
-                className="mb-5"
-              />
-              <ProjectItemInteractingStoragePrice
-                item={item}
-                itemNextValues={itemNextValues}
-                onChangeValue={itemActions.changeStoragePrice}
-                types={moneyTypes}
-                label="Storage price"
-              />
+              <Header size="tiny">Current money type & value</Header>
+              {this.renderCurrentMoney()}
+              <Header size="tiny">All money fields</Header>
+              {this.renderAllMoneyFields()}
             </Grid.Column>
           </Grid.Row>
         </Grid>
