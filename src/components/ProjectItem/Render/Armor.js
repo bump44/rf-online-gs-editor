6b5a3eaa7224f38e445ed72b1fbcca6df763880d
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-13 22:29:20 +0300

    ProjectItemRenderArmor

--- src/components/ProjectItem/Render/Armor.js
+++ src/components/ProjectItem/Render/Armor.js
@@ -4,146 +4,22 @@
  *
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
 import { Map, List } from 'immutable';
-import { Tab } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import SegmentBuilder from './SegmentBuilder';
+
 import SegmentBasicArmor from '../Segment/BasicArmor';
 import SegmentExperts from '../Segment/Experts';
 import SegmentEffects from '../Segment/Effects';
-
-const tabStyle = { height: '100%' };
-const tabMenu = {
-  secondary: true,
-  pointing: true,
-};
+import SegmentActions from '../Segment/Actions';
 
 /* eslint-disable react/prefer-stateless-function */
-class ProjectItemRenderArmor extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.getTabPanes = this.getTabPanes.bind(this);
-    this.renderTabs = this.renderTabs.bind(this);
-    this.renderBasic = this.renderBasic.bind(this);
-    this.renderExperts = this.renderExperts.bind(this);
-    this.renderEffects = this.renderEffects.bind(this);
-  }
-
-  getTabPanes() {
-    return [
-      {
-        menuItem: { key: 'basics', content: 'Basic' },
-        render: this.renderBasic,
-      },
-      {
-        menuItem: { key: 'experts', content: 'Experts' },
-        render: this.renderExperts,
-      },
-      {
-        menuItem: { key: 'effects', content: 'Effects' },
-        render: this.renderEffects,
-      },
-    ];
-  }
-
-  renderExperts() {
-    const {
-      item,
-      itemNextValues,
-      nextValues,
-      boxItemOutActions,
-      entriesFinderItemsActions,
-      entriesFinderItems,
-      itemActions,
-      moneyTypes,
-      itemGradeTypes,
-      weaponTypes,
-      localSettings,
-      expertTypes,
-      effectTypes,
-    } = this.props;
-
-    return (
-      <SegmentExperts
-        item={item}
-        itemNextValues={itemNextValues}
-        nextValues={nextValues}
-        boxItemOutActions={boxItemOutActions}
-        entriesFinderItemsActions={entriesFinderItemsActions}
-        entriesFinderItems={entriesFinderItems}
-        itemActions={itemActions}
-        moneyTypes={moneyTypes}
-        itemGradeTypes={itemGradeTypes}
-        weaponTypes={weaponTypes}
-        localSettings={localSettings}
-        expertTypes={expertTypes}
-        effectTypes={effectTypes}
-      />
-    );
-  }
-
-  renderEffects() {
-    const {
-      item,
-      itemNextValues,
-      nextValues,
-      boxItemOutActions,
-      entriesFinderItemsActions,
-      entriesFinderItems,
-      itemActions,
-      moneyTypes,
-      itemGradeTypes,
-      weaponTypes,
-      localSettings,
-      expertTypes,
-      effectTypes,
-    } = this.props;
-
-    return (
-      <SegmentEffects
-        item={item}
-        itemNextValues={itemNextValues}
-        nextValues={nextValues}
-        boxItemOutActions={boxItemOutActions}
-        entriesFinderItemsActions={entriesFinderItemsActions}
-        entriesFinderItems={entriesFinderItems}
-        itemActions={itemActions}
-        moneyTypes={moneyTypes}
-        itemGradeTypes={itemGradeTypes}
-        weaponTypes={weaponTypes}
-        localSettings={localSettings}
-        expertTypes={expertTypes}
-        effectTypes={effectTypes}
-      />
-    );
-  }
-
-  renderBasic() {
-    const {
-      item,
-      itemNextValues,
-      itemActions,
-      moneyTypes,
-      itemGradeTypes,
-    } = this.props;
-
-    return (
-      <SegmentBasicArmor
-        item={item}
-        itemNextValues={itemNextValues}
-        itemActions={itemActions}
-        moneyTypes={moneyTypes}
-        itemGradeTypes={itemGradeTypes}
-      />
-    );
-  }
-
-  renderTabs() {
-    return <Tab menu={tabMenu} panes={this.getTabPanes()} style={tabStyle} />;
-  }
-
-  render() {
-    return this.renderTabs();
+class ProjectItemRenderArmor extends SegmentBuilder {
+  componentWillMount() {
+    this.addTabPane('basics', 'Basic', SegmentBasicArmor)
+      .addTabPane('experts', 'Experts', SegmentExperts)
+      .addTabPane('effects', 'Effects', SegmentEffects)
+      .addTabPane('actions', 'Actions', SegmentActions);
   }
 }
 
