5b1fcaaa63fa613a4af09627fea35732df203846
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-15 11:26:29 +0300

    map active

---
+++ src/structs/server/map/active_reader.js
@@ -0,0 +1,20 @@
+import FileReader from '../../../classes/FileReader';
+import defaultHeader from './default_header';
+import activeStruct from './active_struct';
+
+export default class ServerMapactiveReader extends FileReader {
+  constructor(props = {}) {
+    super({
+      ...props,
+      name: 'ServerMapactiveReader',
+      struct,
+    });
+  }
+}
+
+export const struct = [
+  {
+    header: defaultHeader,
+    block: activeStruct,
+  },
+];
---
+++ src/structs/server/map/active_struct.js
@@ -0,0 +1,12 @@
+import Struct from '../../../classes/Struct';
+
+export default new Struct().fromSchema1([
+  { child: { type: Number, name: 'nIndex', len: 32 } },
+  { child: { type: String, name: 'strCode', len: 64 } },
+  { child: { type: Number, name: 'nRegenTime', len: 32 } },
+  { child: { type: Number, name: 'nRegenLimNum', len: 32 } },
+  { child: { type: Number, name: 'nRegenProp', len: 32 } },
+  { child: { type: Number, name: 'nRegenMinNum', len: 32 } },
+  { child: { type: Number, name: 'nStdKill', len: 32 } },
+  { child: { type: Number, name: 'nRegenMaxNum', len: 32 } },
+]);
