b3496cf3c5de85a3f9a840a0b6bb48d29616e375
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-24 06:33:04 +0300

    Resolvers

---
+++ src/containers/App/sagas/projectNextValue/mapSptResolvers.js
@@ -0,0 +1,70 @@
+const Resolvers = {
+  a1(mapSpt, value) {
+    return mapSpt.set('a1', value);
+  },
+  a2(mapSpt, value) {
+    return mapSpt.set('a2', value);
+  },
+  a3(mapSpt, value) {
+    return mapSpt.set('a3', value);
+  },
+  a4(mapSpt, value) {
+    return mapSpt.set('a4', value);
+  },
+  a5(mapSpt, value) {
+    return mapSpt.set('a5', value);
+  },
+  a6(mapSpt, value) {
+    return mapSpt.set('a6', value);
+  },
+  b1(mapSpt, value) {
+    return mapSpt.set('b1', value);
+  },
+  b2(mapSpt, value) {
+    return mapSpt.set('b2', value);
+  },
+  b3(mapSpt, value) {
+    return mapSpt.set('b3', value);
+  },
+  b4(mapSpt, value) {
+    return mapSpt.set('b4', value);
+  },
+  c1(mapSpt, value) {
+    return mapSpt.set('c1', value);
+  },
+  c2(mapSpt, value) {
+    return mapSpt.set('c2', value);
+  },
+  c3(mapSpt, value) {
+    return mapSpt.set('c3', value);
+  },
+  c4(mapSpt, value) {
+    return mapSpt.set('c4', value);
+  },
+  d1(mapSpt, value) {
+    return mapSpt.set('d1', value);
+  },
+  d2(mapSpt, value) {
+    return mapSpt.set('d2', value);
+  },
+  d3(mapSpt, value) {
+    return mapSpt.set('d3', value);
+  },
+  d4(mapSpt, value) {
+    return mapSpt.set('d4', value);
+  },
+  e1(mapSpt, value) {
+    return mapSpt.set('e1', value);
+  },
+  e2(mapSpt, value) {
+    return mapSpt.set('e2', value);
+  },
+  e3(mapSpt, value) {
+    return mapSpt.set('e3', value);
+  },
+  e4(mapSpt, value) {
+    return mapSpt.set('e4', value);
+  },
+};
+
+export default Resolvers;
