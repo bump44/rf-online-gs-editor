bd343e2e70c7e5f484ffc0d4cce01fbc577d556d
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-26 18:45:50 +0300

    ProjectStoreInteractingModel

---
+++ src/components/Project/Store/Interacting/Model.js
@@ -0,0 +1,74 @@
+/**
+ *
+ * ProjectStoreInteractingModel
+ *
+ */
+
+import { Input } from 'semantic-ui-react';
+import { Map } from 'immutable';
+
+import { getModel } from '~/containers/App/getters/projectStore';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+class ProjectStoreInteractingModel extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.changeValue = evt => {
+      const { onChangeValue, store } = this.props;
+      onChangeValue(store, evt.target.value);
+    };
+  }
+
+  render() {
+    const {
+      store,
+      storeNextValues,
+      size,
+      className,
+      label,
+      fluid,
+    } = this.props;
+
+    const value = getModel(storeNextValues.get('nextValue'), {
+      entry: store,
+    });
+
+    return (
+      <Input
+        size={size}
+        fluid={fluid}
+        type="text"
+        value={value}
+        onChange={this.changeValue}
+        className={className}
+        label={label}
+      />
+    );
+  }
+}
+
+ProjectStoreInteractingModel.propTypes = {
+  store: PropTypes.instanceOf(Map).isRequired,
+  storeNextValues: PropTypes.instanceOf(Map).isRequired,
+  onChangeValue: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(['mini', 'small', 'large', 'big', 'huge', 'massive']),
+  className: PropTypes.string,
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.node,
+    PropTypes.number,
+  ]),
+  fluid: PropTypes.bool,
+};
+
+ProjectStoreInteractingModel.defaultProps = {
+  size: 'mini',
+  className: '',
+  label: null,
+  fluid: true,
+};
+
+export default ProjectStoreInteractingModel;
