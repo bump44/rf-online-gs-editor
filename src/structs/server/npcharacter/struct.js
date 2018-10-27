af180e03163cdf605605724bda1e9f0be6878b03
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-27 10:18:50 +0300

    fix struct

--- src/structs/server/npcharacter/struct.js
+++ src/structs/server/npcharacter/struct.js
@@ -3,6 +3,7 @@ import Struct from '~/classes/Struct';
 export default new Struct().fromSchema1([
   { child: { type: Number, name: 'nIndex', len: 32 } },
   { child: { type: String, name: 'strCode', len: 64 } },
+  { child: { type: String, name: 'strName', len: 64 } },
   { child: { type: Number, name: 'nRace', len: 32 } },
   { child: { type: Boolean, name: 'bWarType', len: 32 } },
   { child: { type: Number, name: 'fTarDecType', len: 32, as: 'float' } },
