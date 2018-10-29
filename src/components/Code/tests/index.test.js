b4da386cf111287b7ff8073eca55eda182ea2823
Ilya Gerasimov <r0sky1@yandex.ru>
2018-10-30 02:17:54 +0300

    <Code /> tests

--- src/components/Code/tests/index.test.js
+++ src/components/Code/tests/index.test.js
@@ -1,10 +1,24 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import Code from '../index';
+import Code from '../index';
 
 describe('<Code />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  it('Should exist', () => {
+    const wrapper = shallow(<Code />);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('Code__Text').prop('onClick')).toBeInstanceOf(Function);
+  });
+  it('Should have children', () => {
+    const children = 'children';
+    const wrapper = shallow(<Code>{children}</Code>);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('Code__Text').text()).toEqual(children);
+  });
+  it('Should have onClick', () => {
+    const onClick = () => null;
+    const wrapper = shallow(<Code onClick={onClick} />);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('Code__Text').prop('onClick')).toBeInstanceOf(Function);
   });
 });
