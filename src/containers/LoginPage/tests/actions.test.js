8d21f71bb53a91610f59096423c44e2f51085ca9
Ilya Gerasimov <r0sky1@yandex.ru>
2018-11-01 14:16:09 +0300

    add tests

--- src/containers/LoginPage/tests/actions.test.js
+++ src/containers/LoginPage/tests/actions.test.js
@@ -1,5 +1,15 @@
-import { defaultAction } from '../actions';
-import { DEFAULT_ACTION } from '../constants';
+import {
+  defaultAction,
+  changeFieldValue,
+  changeIdent,
+  changePassword,
+  changeIsLoading,
+  changeIsError,
+  changeErrorMessage,
+  submit,
+} from '../actions';
+
+import { DEFAULT_ACTION, CHANGE_FIELD_VALUE, SUBMIT } from '../constants';
 
 describe('LoginPage actions', () => {
   describe('Default Action', () => {
@@ -8,6 +18,74 @@ describe('LoginPage actions', () => {
         type: DEFAULT_ACTION,
       };
       expect(defaultAction()).toEqual(expected);
+    });
+  });
+  describe('Change Field Value Action', () => {
+    it('has a type of CHANGE_FIELD_VALUE', () => {
+      const expected = {
+        type: CHANGE_FIELD_VALUE,
+        key: 'key',
+        value: 'value',
+      };
+      expect(changeFieldValue('key', 'value')).toEqual(expected);
+    });
+  });
+  describe('Change Ident Action', () => {
+    it('has a type of CHANGE_FIELD_VALUE', () => {
+      const expected = {
+        type: CHANGE_FIELD_VALUE,
+        key: 'ident',
+        value: 'value',
+      };
+      expect(changeIdent('value')).toEqual(expected);
+    });
+  });
+  describe('Change Password Action', () => {
+    it('has a type of CHANGE_FIELD_VALUE', () => {
+      const expected = {
+        type: CHANGE_FIELD_VALUE,
+        key: 'password',
+        value: 'value',
+      };
+      expect(changePassword('value')).toEqual(expected);
+    });
+  });
+  describe('Change IsLoading Action', () => {
+    it('has a type of CHANGE_FIELD_VALUE', () => {
+      const expected = {
+        type: CHANGE_FIELD_VALUE,
+        key: 'isLoading',
+        value: true,
+      };
+      expect(changeIsLoading(true)).toEqual(expected);
+    });
+  });
+  describe('Change IsError Action', () => {
+    it('has a type of CHANGE_FIELD_VALUE', () => {
+      const expected = {
+        type: CHANGE_FIELD_VALUE,
+        key: 'isError',
+        value: false,
+      };
+      expect(changeIsError(false)).toEqual(expected);
+    });
+  });
+  describe('Change ErrorMessage Action', () => {
+    it('has a type of CHANGE_FIELD_VALUE', () => {
+      const expected = {
+        type: CHANGE_FIELD_VALUE,
+        key: 'errorMessage',
+        value: 'value',
+      };
+      expect(changeErrorMessage('value')).toEqual(expected);
+    });
+  });
+  describe('Submit Action', () => {
+    it('has a type of CHANGE_FIELD_VALUE', () => {
+      const expected = {
+        type: SUBMIT,
+      };
+      expect(submit()).toEqual(expected);
     });
   });
 });
--- src/containers/LoginPage/tests/saga.test.js
+++ src/containers/LoginPage/tests/saga.test.js
@@ -3,13 +3,14 @@
  */
 
 /* eslint-disable redux-saga/yield-effects */
-// import { take, call, put, select } from 'redux-saga/effects';
-// import { defaultSaga } from '../saga';
+import { all, call } from 'redux-saga/effects';
+import defaultSaga, { watchSubmit } from '../saga';
 
-// const generator = defaultSaga();
+const generator = defaultSaga();
 
 describe('defaultSaga Saga', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  it('should have watchSubmit', () => {
+    const effect = generator.next().value;
+    expect(effect).toEqual(all([call(watchSubmit)]));
   });
 });
