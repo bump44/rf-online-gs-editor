7d1f01430eb88835af3fab23542f5f42cdef2d89
Ilya Gerasimov <r0sky1@yandex.ru>
2018-11-03 19:03:14 +0300

    project export page query

--- src/apollo/queries/project_export_page.js
+++ src/apollo/queries/project_export_page.js
@@ -1,3 +1,50 @@
-import query from './sub/project';
+import gql from 'graphql-tag';
 
-export default query;
+export default gql`
+  query($id: String!) {
+    project(id: $id) {
+      title
+      description
+      name
+      id
+      createdAt
+      updatedAt
+      isPublic
+      owner {
+        id
+        login
+        role {
+          id
+          name
+          title
+        }
+      }
+      items {
+        total
+      }
+      stores {
+        total
+      }
+      itemsBox: items(where: { type: "box" }) {
+        total
+      }
+      boxItemOuts {
+        total
+      }
+      resources {
+        total
+      }
+      mapSpts {
+        total
+      }
+
+      mapNameTypes {
+        items {
+          title
+          value
+          caseSens
+        }
+      }
+    }
+  }
+`;
