ef91f54e48475ced1e88639c5e780dce974942b0
Ilya Gerasimov <r0sky1@yandex.ru>
2018-11-09 12:27:18 +0300

    ProjectPotionItemEffectInteractingNeedItemCount

---
+++ src/components/Project/PotionItemEffect/Interacting/NeedItemCount.js
@@ -0,0 +1,97 @@
+/**
+ *
+ * ProjectPotionItemEffectInteractingNeedItemCount
+ *
+ */
+
+import { Input } from 'semantic-ui-react';
+import { Map } from 'immutable';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { getNeedItemCount } from '~/containers/App/getters/projectPotionItemEffect';
+import ProjectPotionItemEffectFieldFormattedMessage from '../FieldFormattedMessage';
+
+/* eslint-disable react/prefer-stateless-function */
+class ProjectPotionItemEffectInteractingNeedItemCount extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.renderInput = this.renderInput.bind(this);
+    this.changeValue = evt => {
+      const { onChangeValue, potionItemEffect, n } = this.props;
+      onChangeValue(potionItemEffect, {
+        value: parseInt(evt.target.value, 10) || 0,
+        n,
+      });
+    };
+  }
+
+  renderInput(message) {
+    const {
+      potionItemEffect,
+      potionItemEffectNextValues,
+      className,
+      label,
+      fluid,
+      size,
+      withDefaultLabel,
+      n,
+    } = this.props;
+
+    const value = getNeedItemCount(
+      potionItemEffectNextValues.get('nextValue'),
+      { entry: potionItemEffect },
+      { n },
+    );
+
+    const labelProp = label || withDefaultLabel ? label || message : undefined;
+
+    return (
+      <Input
+        label={labelProp}
+        fluid={fluid}
+        size={size}
+        value={value}
+        onChange={this.changeValue}
+        className={className}
+        min="0"
+        type="number"
+      />
+    );
+  }
+
+  render() {
+    return (
+      <ProjectPotionItemEffectFieldFormattedMessage message="NeedItemCount">
+        {this.renderInput}
+      </ProjectPotionItemEffectFieldFormattedMessage>
+    );
+  }
+}
+
+ProjectPotionItemEffectInteractingNeedItemCount.propTypes = {
+  potionItemEffect: PropTypes.instanceOf(Map).isRequired,
+  potionItemEffectNextValues: PropTypes.instanceOf(Map).isRequired,
+  onChangeValue: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(['mini', 'small', 'large', 'big', 'huge', 'massive']),
+  className: PropTypes.string,
+  fluid: PropTypes.bool,
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.node,
+    PropTypes.number,
+  ]),
+  withDefaultLabel: PropTypes.bool,
+  n: PropTypes.number.isRequired,
+};
+
+ProjectPotionItemEffectInteractingNeedItemCount.defaultProps = {
+  size: 'mini',
+  className: '',
+  label: null,
+  fluid: true,
+  withDefaultLabel: true,
+};
+
+export default ProjectPotionItemEffectInteractingNeedItemCount;
