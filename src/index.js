d2d0151d4463973a898fe378bd2de7310934a3f6
Ilya Gerasimov <r0sky1@yandex.ru>
2018-11-29 14:55:13 +0300

    remove dev

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.webContents.openDevTools();
+
   if (isDevMode) {
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
