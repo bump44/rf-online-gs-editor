10c772bae24cd762570e7e5b52847c887788f8ed
Ilya Gerasimov <r0sky1@yandex.ru>
2018-12-09 03:34:59 +0300

    update Fullheight component tests

--- src/components/FullheightColumn/tests/index.test.js
+++ src/components/FullheightColumn/tests/index.test.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import 'jest-styled-components';
 import { shallow } from 'enzyme';
+import React from 'react';
+import renderer from 'react-test-renderer';
 
-import FullheightColumn, { FullheightThis } from '../index';
+import FullheightColumn, {
+  FullheightThis,
+  FullheightAutoSizer,
+  FullheightAutoSizerChild,
+} from '../index';
 
 describe('<FullheightColumn />', () => {
-  it('Should exist', () => {
+  // test styled
+  it('should exist FullheightThis', () => {
+    const rendered = renderer.create(<FullheightThis />).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+  it('should render FullheightThis with custom props', () => {
+    const rendered = renderer
+      .create(<FullheightThis paddingTop={10} paddingBottom={99} />)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+  it('should exist FullheightAutoSizerChild', () => {
+    const rendered = renderer.create(<FullheightAutoSizerChild />).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+  it('should render FullheightAutoSizerChild with custom props', () => {
+    const rendered = renderer
+      .create(<FullheightAutoSizerChild width={10} height={99} />)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+  it('should exist FullheightAutoSizer', () => {
+    const wrapper = shallow(
+      <FullheightAutoSizer>
+        <div />
+      </FullheightAutoSizer>,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should exist FullheightColumn', () => {
     const wrapper = shallow(
       <FullheightColumn>
         <div />
       </FullheightColumn>,
-    );
-    expect(wrapper).toMatchSnapshot();
-  });
-  it('Should exist FullheightThis', () => {
-    const wrapper = shallow(
-      <FullheightThis>
-        <div />
-      </FullheightThis>,
     );
     expect(wrapper).toMatchSnapshot();
   });
